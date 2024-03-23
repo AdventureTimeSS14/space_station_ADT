5a57d691983a8ad2eefa9d84665b46cecdcd52cc
Rxup <backmen@bk.ru>
2024-03-23 18:27:16 +0300

    Disease #530

---
+++ Content.Shared/Backmen/CCVar/CCVars.cs
@@ -0,0 +1,12 @@
+using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Shared.Backmen.CCVar;
+
+// ReSharper disable once InconsistentNaming
+[CVarDefs]
+public sealed class CCVars
+{
+    public static readonly CVarDef<bool>
+        GameDiseaseEnabled = CVarDef.Create("game.disease", true, CVar.SERVERONLY);
+}
