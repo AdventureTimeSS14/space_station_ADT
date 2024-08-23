d7bb906a96d6e884db6433e95e8a3a6751a57096
FaDeOkno <143940725+FaDeOkno@users.noreply.github.com>
2024-08-23 21:24:37 +0400

    Защита от стамина урона + блокировка шприцов ухтантам и скафандрам (#314)
    <!-- ЭТО ШАБЛОН ВАШЕГО PULL REQUEST. Текст между стрелками - это
    комментарии - они не будут видны в PR. -->
    
    ## Описание PR
    <!-- Ниже опишите ваш Pull Request. Что он изменяет? На что еще это
    может повлиять? Постарайтесь описать все внесённые вами изменения! -->
    Добавил защиту от стана и блокировку шприцов унатхам, а так же мобам в
    скафандрах
    
    **Медиа**
    <!-- Если приемлемо, добавьте скриншоты для демонстрации вашего PR. Если
    ваш PR представляет собой визуальное изменение, добавьте
    скриншоты, иначе он может быть закрыт. -->
    
    **Проверки**
    <!-- Выполнение всех следующих действий, если это приемлемо для вида
    изменений сильно ускорит разбор вашего PR -->
    - [ ] PR полностью завершён и мне не нужна помощь чтобы его закончить.
    - [ ] Я внимательно просмотрел все свои изменения и багов в них не
    нашёл.
    - [ ] Я запускал локальный сервер со своими изменениями и всё
    протестировал.
    - [ ] Я добавил скриншот/видео демонстрации PR в игре, **или** этот PR
    этого не требует.
    
    **Изменения**
    <!--
    Здесь вы можете написать список изменений, который будет автоматически
    добавлен в игру, когда ваш PR будет принят.
    
    В журнал изменений следует помещать только то, что действительно важно
    игрокам.
    
    В списке изменений тип значка не является часть предложения, поэтому
    явно указывайте - Добавлен, Удалён, Изменён.
    плохо: - add: Новый инструмент для инженеров
    хорошо: - add: Добавлен новый инструмент для инженеров
    
    Вы можете указать своё имя после символа :cl: именно оно будет
    отображаться в журнале изменений (иначе будет использоваться ваше имя на
    GitHub)
    Например: :cl: Ian
    
    -->
    
    :cl: Котя
    - add: Добавлена защита от стана скафандрам ЯО и СБ.
    - add: Теперь шприцы не работают на мобов, которые носят скафандры.
    - add: Шприцы так же перестали работать на унатхов даже без скафандра.
    
    ---------
    
    Co-authored-by: bananchiki <87230164+Darkiich@users.noreply.github.com>

---
+++ Content.Shared/ADT/Chemistry/Components/InjectorsBlockerComponent.cs
@@ -0,0 +1,12 @@
+using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.DoAfter;
+using Content.Shared.FixedPoint;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Chemistry.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class InjectorsBlockerComponent : Component
+{
+}
--- Content.Shared/Armor/SharedArmorSystem.cs
+++ Content.Shared/Armor/SharedArmorSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
 using Content.Shared.Examine;
 using Content.Shared.Inventory;
 using Content.Shared.Silicons.Borgs;
@@ -22,6 +23,7 @@ public abstract class SharedArmorSystem : EntitySystem
         SubscribeLocalEvent<ArmorComponent, InventoryRelayedEvent<DamageModifyEvent>>(OnDamageModify);
         SubscribeLocalEvent<ArmorComponent, BorgModuleRelayedEvent<DamageModifyEvent>>(OnBorgDamageModify);
         SubscribeLocalEvent<ArmorComponent, GetVerbsEvent<ExamineVerb>>(OnArmorVerbExamine);
+        SubscribeLocalEvent<ArmorComponent, InventoryRelayedEvent<StaminaDamageModifyEvent>>(OnStaminaDamageModify);    // ADT Stunmeta fix
     }
 
     private void OnDamageModify(EntityUid uid, ArmorComponent component, InventoryRelayedEvent<DamageModifyEvent> args)
@@ -40,7 +42,7 @@ public abstract class SharedArmorSystem : EntitySystem
         if (!args.CanInteract || !args.CanAccess)
             return;
 
-        var examineMarkup = GetArmorExamine(component.Modifiers);
+        var examineMarkup = GetArmorExamine(component.Modifiers, component.StaminaModifier);    // ADT Stunmeta fix
 
         var ev = new ArmorExamineEvent(examineMarkup);
         RaiseLocalEvent(uid, ref ev);
@@ -50,7 +52,7 @@ public abstract class SharedArmorSystem : EntitySystem
             Loc.GetString("armor-examinable-verb-message"));
     }
 
-    private FormattedMessage GetArmorExamine(DamageModifierSet armorModifiers)
+    private FormattedMessage GetArmorExamine(DamageModifierSet armorModifiers, float staminaModifier)   // ADT Stunmeta fix
     {
         var msg = new FormattedMessage();
         msg.AddMarkupOrThrow(Loc.GetString("armor-examine"));
@@ -77,6 +79,18 @@ public abstract class SharedArmorSystem : EntitySystem
             ));
         }
 
+        // ADT Stunmeta fix start
+        msg.PushNewline();
+        msg.AddMarkup(Loc.GetString("armor-stamina-protection-value", ("value", MathF.Round((1f - staminaModifier) * 100, 1))));
+        // ADT Stunmeta fix end
+
         return msg;
     }
+
+    // ADT Stunmeta fix start
+    private void OnStaminaDamageModify(EntityUid uid, ArmorComponent component, InventoryRelayedEvent<StaminaDamageModifyEvent> args)
+    {
+        args.Args.Damage *= component.StaminaModifier;
+    }
+    // ADT Stunmeta fix end
 }
