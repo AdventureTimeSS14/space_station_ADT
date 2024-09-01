ad1f5422e8d3858de97afeff43636a455f8d434c
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-09-01 17:15:34 +0300

    SayPosting (#220)
    <!-- ЭТО ШАБЛОН ВАШЕГО PULL REQUEST. Текст между стрелками - это
    комментарии - они не будут видны в PR. -->
    
    ## Описание PR
    <!-- Ниже опишите ваш Pull Request. Что он изменяет? На что еще это
    может повлиять? Постарайтесь описать все внесённые вами изменения! -->
    Вешаем один из этих компонентов на сущность и она начинает постить в
    зависимости от компоненты или эмоуты или слова
    
    
    **Проверки**
    <!-- Выполнение всех следующих действий, если это приемлемо для вида
    изменений сильно ускорит разбор вашего PR -->
    - [ ] PR полностью завершён и мне не нужна помощь чтобы его закончить.
    - [x] Я внимательно просмотрел все свои изменения и багов в них не
    нашёл.
    - [x] Я запускал локальный сервер со своими изменениями и всё
    протестировал.
    - [ ] Я добавил скриншот/видео демонстрации PR в игре, **или** этот PR
    этого не требует.
    
    **Изменения**
    no cl, no fun

---
+++ Content.Server/ADT/AutoPostingChat/AutoSpeakPostingChatSystem.cs
@@ -0,0 +1,51 @@
+using Content.Shared.Mobs;
+using Content.Server.Chat.Systems;
+using Content.Shared.ADT.AutoPostingChat;
+using Robust.Shared.Timing;
+using Robust.Shared.Random;
+public sealed class AutoSpeakPostingChatSystem : EntitySystem
+{
+    [Dependency] private readonly ChatSystem _chat = default!;
+    [Dependency] private readonly IGameTiming _time = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<AutoSpeakPostingChatComponent, MobStateChangedEvent>(OnMobState);
+    }
+    /// <summary>
+    /// On death removes active comps and gives genetic damage to prevent cloning, reduce this to allow cloning.
+    /// </summary>
+    private void OnMobState(EntityUid uid, AutoSpeakPostingChatComponent component, MobStateChangedEvent args)
+    {
+        if (args.NewMobState == MobState.Dead)
+        {
+            RemComp<AutoSpeakPostingChatComponent>(uid);
+        }
+    }
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<AutoSpeakPostingChatComponent>();
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (_time.CurTime >= comp.NextSecond)
+            {
+                var delay = comp.SpeakTimerRead;
+
+                if (comp.PostingMessageSpeak != null)
+                {
+                    _chat.TrySendInGameICMessage(uid, comp.PostingMessageSpeak, InGameICChatType.Speak, ChatTransmitRange.Normal);
+                }
+
+                if (comp.RandomIntervalSpeak)
+                {
+                    delay = _random.Next(comp.IntervalRandomSpeakMin, comp.IntervalRandomSpeakMax);
+                }
+
+                comp.NextSecond = _time.CurTime + TimeSpan.FromSeconds(delay);
+            }
+        }
+    }
+}
