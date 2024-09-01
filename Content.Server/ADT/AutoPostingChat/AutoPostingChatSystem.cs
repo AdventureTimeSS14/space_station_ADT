9be4067ffab99a39d7f9e57ddb5f04316e59c070
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-09-01 10:54:57 +0300

    Revert "ага опять"
    This reverts commit 5509fe92b898fecec39668eb44665dc71642ae91.

--- Content.Server/ADT/AutoPostingChat/AutoPostingChatSystem.cs
+++ Content.Server/ADT/AutoPostingChat/AutoPostingChatSystem.cs
@@ -1,8 +1,12 @@
 using Content.Server.Administration.Commands;
+using Content.Server.Popups;
+using Content.Shared.Popups;
 using Content.Shared.Mobs;
 using Content.Server.Chat;
 using Content.Server.Chat.Systems;
 using Content.Shared.Chat.Prototypes;
+using Robust.Shared.Random;
+using Content.Shared.Stunnable;
 using Content.Shared.Damage.Prototypes;
 using Content.Shared.Damage;
 using Robust.Shared.Prototypes;
@@ -10,117 +14,136 @@ using Content.Server.Emoting.Systems;
 using Content.Server.Speech.EntitySystems;
 using Content.Shared.ADT.AutoPostingChat;
 using Content.Shared.Interaction.Components;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
 using System.Timers;
 using System.ComponentModel;
 using System.Linq;
 using Robust.Shared.Timing;
-using Robust.Shared.Random;
+using Robust.Shared.Utility;
 public sealed class AutoPostingChatSystem : EntitySystem
 {
-    // [Dependency] private readonly DamageableSystem _damageableSystem = default!;
-    // [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly DamageableSystem _damageableSystem = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly ChatSystem _chat = default!;
-    [Dependency] private readonly IRobustRandom _random = default!;
-    private System.Timers.Timer _speakTimer = new();
-    private System.Timers.Timer _emoteTimer = new();
-    //private readonly Random _random = new Random(); 
-    //private static readonly Random _random = new Random();
-
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<AutoPostingChatComponent, ComponentStartup>(OnComponentStartup);
         SubscribeLocalEvent<AutoPostingChatComponent, MobStateChangedEvent>(OnMobState);
-        SubscribeLocalEvent<AutoPostingChatComponent, ComponentShutdown>(ComponentRemove);
     }
-
-    private void ComponentRemove(EntityUid uid, AutoPostingChatComponent component, ComponentShutdown args)
-    {
-        _speakTimer.Stop();
-        _emoteTimer.Stop();
-        _speakTimer.Dispose(); // освобождаем ресурсы
-        _emoteTimer.Dispose();
-    }
-
     /// <summary>
-    /// On death removes active comps.
+    /// On death removes active comps and gives genetic damage to prevent cloning, reduce this to allow cloning.
     /// </summary>
     private void OnMobState(EntityUid uid, AutoPostingChatComponent component, MobStateChangedEvent args)
     {
-        if (args.NewMobState == MobState.Dead)
+        if (args.NewMobState == MobState.Dead || component == null)
         {
             RemComp<AutoPostingChatComponent>(uid);
+            var damageSpec = new DamageSpecifier(_prototypeManager.Index<DamageGroupPrototype>("Genetic"), 300);
+            _damageableSystem.TryChangeDamage(uid, damageSpec);
         }
     }
-
     private void OnComponentStartup(EntityUid uid, AutoPostingChatComponent component, ComponentStartup args)
     {
-        SetupSpeakTimer(uid, component);
-        SetupEmoteTimer(uid, component);
-    }
-
-    private void SetupSpeakTimer(EntityUid uid, AutoPostingChatComponent component)
-    {
-        //var msg = _random.Pick(component.SpeakTimerRead);
-        _speakTimer.Interval = component.RandomIntervalSpeak ? _random.Next(component.MinRandomIntervalSpeak*1000, component.MaxRandomIntervalSpeak*1000) : component.SpeakTimerRead*1000;
-        _speakTimer.Elapsed += (sender, eventArgs) =>
+        // Проверяем наличие компонента AutoPostingChatComponent на сущности
+        //if (component == null)
+        //{
+        //    Logger.Warning("AutoPostingChatComponent отсутствует на сущности с UID: " + uid);
+        //    return;
+        //}
+        // Создаем таймеры для Speak и Emote
+        var speakTimer = new System.Timers.Timer(component.SpeakTimerRead); // 8000 миллисекунд = 8 секунд по умолчанию
+        speakTimer.Elapsed += (sender, eventArgs) =>
         {
+            // Проверяем, что данные в компоненте были обновлены
             if (component.PostingMessageSpeak != null)
             {
-                _chat.TrySendInGameICMessage(uid, _random.Pick(component.PostingMessageSpeak), 
-                InGameICChatType.Speak, ChatTransmitRange.Normal);
+                //if (component.PostingMessageSpeak == "")
+                //    speakTimer.Stop();
+
+                _chat.TrySendInGameICMessage(uid, component.PostingMessageSpeak, InGameICChatType.Speak, ChatTransmitRange.Normal);
             }
-            _speakTimer.Interval = component.RandomIntervalSpeak ? _random.Next(component.MinRandomIntervalSpeak*1000, component.MaxRandomIntervalSpeak*1000) : component.SpeakTimerRead*1000;
         };
-
-        _speakTimer.Start();
-    }
-
-    private void SetupEmoteTimer(EntityUid uid, AutoPostingChatComponent component)
-    {
-        _emoteTimer.Interval = component.RandomIntervalEmote ? _random.Next(component.MinRandomIntervalEmote*1000, component.MaxRandomIntervalEmote*1000) : component.EmoteTimerRead*1000;
-        _emoteTimer.Elapsed += (sender, eventArgs) =>
+        var emoteTimer = new System.Timers.Timer(component.EmoteTimerRead); // 9000 миллисекунд = 9 секунд по умолчанию
+        emoteTimer.Elapsed += (sender, eventArgs) =>
         {
+            // Проверяем, что данные в компоненте были обновлены
             if (component.PostingMessageEmote != null)
             {
-                _chat.TrySendInGameICMessage(uid, _random.Pick(component.PostingMessageEmote), 
-                InGameICChatType.Emote, ChatTransmitRange.Normal);
+                //if (component.PostingMessageEmote == "")
+                //    emoteTimer.Stop();
+                _chat.TrySendInGameICMessage(uid, component.PostingMessageEmote, InGameICChatType.Emote, ChatTransmitRange.Normal);
             }
-           _emoteTimer.Interval = component.RandomIntervalEmote ? _random.Next(component.MinRandomIntervalEmote*1000, component.MaxRandomIntervalEmote*1000) : component.EmoteTimerRead*1000;
         };
-
-        _emoteTimer.Start();
+        // Запускаем таймеры
+        speakTimer.Start();
+        emoteTimer.Start();
     }
 
-    // private void OnComponentStartup(EntityUid uid, AutoPostingChatComponent component, ComponentStartup args)
-    // {
-    //     if (component == null)
-    //     {
-    //         Log.Debug("AutoPostingChatComponent отсутствует на сущности с UID: " + uid);
-    //         return;
-    //     }
+    //// Метод для обновления времени таймера Emote
+    //public void UpdateEmoteTimer(int newTimeInSeconds)
+    //{
+    //    emoteTimer.Interval = newTimeInSeconds * 1000; // Преобразуем секунды в миллисекунды
+    //}
 
-    //     _speakTimer.Interval = component.RandomIntervalSpeak ? _random.Next(1000, 30001) : component.SpeakTimerRead;
-    //     _speakTimer.Elapsed += (sender, eventArgs) =>
-    //     {
-    //         if (component.PostingMessageSpeak != null)
-    //         {
-    //             _chat.TrySendInGameICMessage(uid, component.PostingMessageSpeak, InGameICChatType.Speak, ChatTransmitRange.Normal);
-    //         }
-    //         _speakTimer.Interval = component.RandomIntervalSpeak ? _random.Next(1000, 30001) : component.SpeakTimerRead;
-    //     };
-
-    //     _emoteTimer.Interval = component.RandomIntervalEmote ? _random.Next(1000, 30001) : component.EmoteTimerRead;
-    //     _emoteTimer.Elapsed += (sender, eventArgs) =>
-    //     {
-    //         if (component.PostingMessageEmote != null)
-    //         {
-    //             _chat.TrySendInGameICMessage(uid, component.PostingMessageEmote, InGameICChatType.Emote, ChatTransmitRange.Normal);
-    //         }
-    //         _emoteTimer.Interval = component.RandomIntervalEmote ? _random.Next(1000, 30001) : component.EmoteTimerRead;
-    //     };
-
-    //     _speakTimer.Start();
-    //     _emoteTimer.Start();
-    // }
 }
+//public EmoteSoundsPrototype? EmoteSounds;
+///// <summary>
+///// OnStartup gives the AutoPostingChat outfit, ensures clumsy, gives name prefix and makes sure emote sounds are laugh.
+///// </summary>
+//private void OnComponentStartup(EntityUid uid, AutoPostingChatComponent component, ComponentStartup args)
+//{
+//    //if (component.EmoteSoundsId == null)
+//    //    return;
+//    //_prototypeManager.TryIndex(component.EmoteSoundsId, out EmoteSounds);
+//    //var meta = MetaData(uid);
+//    //var name = meta.EntityName;
+//    //EnsureComp<AutoEmoteComponent>(uid);
+//    //_autoEmote.AddEmote(uid, component.PostingMessageEmote);
+//    //_autoEmote.AddEmote(uid, component.PostingMessageEmote);
+//    //EnsureComp<ClumsyComponent>(uid);
+//    _chat.TrySendInGameICMessage(uid, component.PostingMessageSpeak, InGameICChatType.Speak, ChatTransmitRange.Normal);
+//    _chat.TrySendInGameICMessage(uid, component.PostingMessageEmote, InGameICChatType.Emote, ChatTransmitRange.Normal);
+//}
+
+//private void OnComponentStartup(EntityUid uid, AutoPostingChatComponent component, ComponentStartup args)
+//{
+//    // Проверяем наличие компонента AutoPostingChatComponent на сущности
+//    //if (component == null)
+//    //{
+//    //    Logger.Warning("AutoPostingChatComponent отсутствует на сущности с UID: " + uid);
+//    //    return;
+//    //}
+
+//    // Создаем таймеры для Speak и Emote
+//    var speakTimer = new System.Timers.Timer(component.SpeakTimerRead); // 8000 миллисекунд = 8 секунд
+//    speakTimer.Elapsed += (sender, eventArgs) =>
+//    {
+//        // Проверяем, что данные в компоненте были обновлены
+//        if (component.PostingMessageSpeak != null)
+//        {
+//            //if (component.PostingMessageSpeak == "")
+//            //    speakTimer.Stop();
+
+//            _chat.TrySendInGameICMessage(uid, component.PostingMessageSpeak, InGameICChatType.Speak, ChatTransmitRange.Normal);
+//        }
+//    };
+//    var emoteTimer = new System.Timers.Timer(component.EmoteTimerRead); // 9000 миллисекунд = 9 секунд
+//    emoteTimer.Elapsed += (sender, eventArgs) =>
+//    {
+//        // Проверяем, что данные в компоненте были обновлены
+//        if (component.PostingMessageEmote != null)
+//        {
+//            //if (component.PostingMessageEmote == "")
+//            //    emoteTimer.Stop();
+
+//            _chat.TrySendInGameICMessage(uid, component.PostingMessageEmote, InGameICChatType.Emote, ChatTransmitRange.Normal);
+//        }
+//    };
+//    // Запускаем таймеры
+//    speakTimer.Start();
+//    emoteTimer.Start();
+//}
+
+//private System.Timers.Timer emoteTimer;
--- Content.Shared/ADT/AutoPostingChat/AutoPostingChatComponent.cs
+++ Content.Shared/ADT/AutoPostingChat/AutoPostingChatComponent.cs
@@ -2,74 +2,38 @@ using Robust.Shared.Audio;
 using Content.Shared.Chat.Prototypes;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
 namespace Content.Shared.ADT.AutoPostingChat;
+
 [RegisterComponent]
 [NetworkedComponent]
 public sealed partial class AutoPostingChatComponent : Component
 {
     /// <summary>
-    ///Sets a random interval after each iteration of spoken messages
+    /// timings for giggles and knocks.
     /// </summary>
-    [DataField("randomIntervalSpeak"), ViewVariables(VVAccess.ReadWrite)]
-    public bool RandomIntervalSpeak = false;
+    //[ViewVariables(VVAccess.ReadWrite)]
+    //public TimeSpan DamageGiggleCooldown = TimeSpan.FromSeconds(2);
 
-    /// <summary>
-    /// The interval in milliseconds between automatic speech messages.
-    /// </summary>
+    //[ViewVariables(VVAccess.ReadWrite)]
+    //public float KnockChance = 0.05f;
+    //[ViewVariables(VVAccess.ReadWrite)]
+    //public float GiggleRandomChance = 0.3f;
+
+
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("maxRandomIntervalSpeak")]
-    public int MaxRandomIntervalSpeak = 30;
+    public int SpeakTimerRead = 8000;
 
-    /// <summary>
-    /// The interval in milliseconds between automatic speech messages.
-    /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("minRandomIntervalSpeak")]
-    public int MinRandomIntervalSpeak = 1;
+    public int EmoteTimerRead = 9000;
 
-    /// <summary>
-    /// The interval in milliseconds between automatic speech messages.
-    /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("speakTimer")]
-    public int SpeakTimerRead = 8;
-
-    /// <summary>
-    /// Sets a random interval after each iteration of spoken emotions
-    /// </summary>
-    [DataField("randomIntervalEmote"), ViewVariables(VVAccess.ReadWrite)]
-    public bool RandomIntervalEmote = false;
-
-    /// <summary>
-    /// The interval in milliseconds between automatic speech messages.
-    /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("maxRandomIntervalEmote")]
-    public int MaxRandomIntervalEmote = 30;
-
-    /// <summary>
-    /// The interval in milliseconds between automatic speech messages.
-    /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("minRandomIntervalEmote")]
-    public int MinRandomIntervalEmote = 1;
-
-    /// <summary>
-    /// The interval in milliseconds between automatic emote messages.
-    /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField("emoteTimer")]
-    public int EmoteTimerRead = 9;
-
-    /// <summary>
-    /// The message that will be automatically spoken by the entity.
-    /// </summary>
     [DataField("speakMessage")]
-    public List<string> PostingMessageSpeak = new List<string>();
+    public string? PostingMessageSpeak = "Вульп-вульп!";
 
-    /// <summary>
-    /// The message that will be automatically emotes by the entity.
-    /// </summary>
+
     [DataField("emoteMessage")]
-    public List<string> PostingMessageEmote = new List<string>();
+    public string? PostingMessageEmote = "Кхе";
+
+
+
 }
