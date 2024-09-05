fc3887d09fb1f31cd56b23a260c1c05e84975b26
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-09-05 18:27:58 +0300

    Описание компонента

--- Content.Shared/ADT/NoShowFov/NoShowFovComponent.cs
+++ Content.Shared/ADT/NoShowFov/NoShowFovComponent.cs
@@ -2,5 +2,8 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared.ADT.NoShowFov;
 
+/// <summary>
+///     Applies a fog of war effect to a unit when this component is equipped to the eyes, head, or mask slot.
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class NoShowFovComponent : Component { }
