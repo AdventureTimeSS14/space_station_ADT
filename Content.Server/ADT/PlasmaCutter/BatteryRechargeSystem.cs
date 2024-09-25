73b2268a9765bcde550c8de6c1287f790dcd138f
Korol_Charodey <ferrum2015@gmail.com>
2024-09-25 23:40:39 +0300

    Fixed plasma cutter

--- Content.Server/ADT/PlasmaCutter/BatteryRechargeSystem.cs
+++ Content.Server/ADT/PlasmaCutter/BatteryRechargeSystem.cs
@@ -4,7 +4,6 @@ using Content.Server.Materials;
 using Content.Shared.Materials;
 using Content.Server.Power.EntitySystems;
 using Content.Server.Power.Components;
-using Content.Shared.Weapons.Ranged.Components;
 
 namespace Content.Server.AruMoon.Plasmacutter
 {
