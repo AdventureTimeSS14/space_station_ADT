1dbb88b258f7bae80d36f6def906c505eb57fbf0
Schrodinger71 <sanya.podshibyakin@mail.ru>
2024-10-01 13:21:33 +0300

    вв

--- Content.Server/ADT/Administration/Commands/BanMassCommand.cs
+++ Content.Server/ADT/Administration/Commands/BanMassCommand.cs
@@ -28,7 +28,6 @@ public sealed class BanMassCommand : LocalizedCommands
 
     public override async void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        string[] targets;
         string reason;
         uint minutes;
 
@@ -39,34 +38,26 @@ public sealed class BanMassCommand : LocalizedCommands
             severity = NoteSeverity.High;
         }
 
-        if (args.Length < 2 || args.Length > 3)
+        if (args.Length < 3)
         {
             shell.WriteLine(Loc.GetString("cmd-ban-invalid-arguments"));
             shell.WriteLine(Help);
             return;
         }
 
-        targets = args[0].Split(' ');
-        reason = args[1];
-
-        if (args.Length == 3)
+        reason = args[0];
+        if (!uint.TryParse(args[1], out minutes))
         {
-            if (!uint.TryParse(args[2], out minutes))
-            {
-                shell.WriteLine(Loc.GetString("cmd-ban-invalid-minutes", ("minutes", args[2])));
-                shell.WriteLine(Help);
-                return;
-            }
-        }
-        else
-        {
-            minutes = 0;
+            shell.WriteLine(Loc.GetString("cmd-ban-invalid-minutes", ("minutes", args[1])));
+            shell.WriteLine(Help);
+            return;
         }
 
         var player = shell.Player;
 
-        foreach (var target in targets)
+        for (int i = 2; i < args.Length; i++)
         {
+            var target = args[i]; // Get the current target string
             var located = await _locator.LookupIdByNameOrIdAsync(target);
 
             if (located == null)
@@ -84,17 +75,35 @@ public sealed class BanMassCommand : LocalizedCommands
 
     public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
     {
-        if (args.Length == 1)
+        if (args.Length == 3)
         {
             var options = _playerManager.Sessions.Select(c => c.Name).OrderBy(c => c).ToArray();
             return CompletionResult.FromHintOptions(options, LocalizationManager.GetString("cmd-ban-hint"));
         }
 
         // Если есть пробелы в первом аргументе, также возвращаем подсказки, нужно чтобы если нужно забанить сразу нескольких игроков которые на сервере
-        if (args.Length == 1 && args[0].Contains(' '))
+        if (args.Length == 3 && args[2].Contains(' '))
         {
             var options = _playerManager.Sessions.Select(c => c.Name).OrderBy(c => c).ToArray();
             return CompletionResult.FromHintOptions(options, LocalizationManager.GetString("cmd-ban-hint"));
+        }
+
+        if (args.Length == 1)
+            return CompletionResult.FromHint(LocalizationManager.GetString("cmd-ban-hint-reason"));
+
+        if (args.Length == 2)
+        {
+            var durations = new CompletionOption[]
+            {
+                new("0", LocalizationManager.GetString("cmd-ban-hint-duration-1")),
+                new("1440", LocalizationManager.GetString("cmd-ban-hint-duration-2")),
+                new("4320", LocalizationManager.GetString("cmd-ban-hint-duration-3")),
+                new("10080", LocalizationManager.GetString("cmd-ban-hint-duration-4")),
+                new("20160", LocalizationManager.GetString("cmd-ban-hint-duration-5")),
+                new("43800", LocalizationManager.GetString("cmd-ban-hint-duration-6")),
+            };
+
+            return CompletionResult.FromHintOptions(durations, LocalizationManager.GetString("cmd-ban-hint-duration"));
         }
 
         return CompletionResult.Empty;
