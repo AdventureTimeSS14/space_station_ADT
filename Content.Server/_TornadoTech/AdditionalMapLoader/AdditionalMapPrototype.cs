9997065f07584f41f31e2a8f570a6dc4f19b97dc
Eugeny <klochckov.wot@yandex.ru>
2024-10-30 05:31:44 +0400

    функция загрузки нескольких карт на одну игровую карту

---
+++ Content.Server/_TornadoTech/AdditionalMapLoader/AdditionalMapPrototype.cs
@@ -0,0 +1,14 @@
+﻿using Content.Server.Maps;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._TornadoTech.AdditionalMapLoader;
+
+[Prototype("additionalMap")]
+public sealed class AdditionalMapPrototype : IPrototype
+{
+    [IdDataField]
+    public string ID { get; } = string.Empty;
+
+    [DataField("maps")]
+    public List<ProtoId<GameMapPrototype>> MapProtoIds = new();
+}
