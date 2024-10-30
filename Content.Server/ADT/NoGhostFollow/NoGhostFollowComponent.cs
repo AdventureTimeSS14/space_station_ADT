961e0ba8de6c395839110afabb6e4a29cc53614e
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-10-31 00:35:11 +0300

    идин нахуй

---
+++ Content.Server/ADT/NoGhostFollow/NoGhostFollowComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Server.NoGhostFollow;
+
+/// <summary>
+///  Вешаем на сущность, и к ней нельзя будет тепнуться через "Телепорт призрака".
+/// </summary>
+[RegisterComponent]
+public sealed partial class NoGhostFollowComponent : Component
+{
+}
