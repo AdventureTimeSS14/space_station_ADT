af7b482965fb3a3e070a80a198b1889c9e7709da
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-11-01 10:43:44 +0300

    заготовка

--- Content.Server/ADT/Administration/Commands/EchoChatCommand.cs
+++ Content.Server/ADT/Administration/Commands/EchoChatCommand.cs
@@ -1,60 +1,79 @@
-// using System.Diagnostics.CodeAnalysis;
-// using Content.Server.Administration;
-// using Content.Shared.Access.Components;
-// using Content.Shared.Administration;
-// using Robust.Server.Player;
-// using Robust.Shared.Console;
+using System.Diagnostics.CodeAnalysis;
+using Content.Server.Administration;
+using Content.Shared.Administration.Logs;
+using Content.Shared.Administration;
+using Robust.Server.Player;
+using Robust.Shared.Console;
+using Content.Server.Chat.Systems;
+using Serilog;
+using FastAccessors;
 
-// namespace Content.Server.Mind.Commands;
+namespace Content.Server.ADT.Administration.Commands;
 
-// [AdminCommand(AdminFlags.VarEdit)]
-// public sealed class EchoChatCommand : LocalizedEntityCommands
-// {
-//     [Dependency] private readonly IEntityManager _entManager = default!;
-//     [Dependency] private readonly IPlayerManager _playerManager = default!;
-//     [Dependency] private readonly MetaDataSystem _metaSystem = default!;
+[AdminCommand(AdminFlags.Fun)]
 
-//     public override string Command => "rename";
+// Команда для постинга за персонажа игрока
+// Автор: Discord: schrodinger71
+// Просто фановая команда, что по указанию никнейма, будет постить ваши введённые строки за игрового персонажа указанного никнеймом.
+public sealed class EchoChatCommand : LocalizedEntityCommands
+{
+    [Dependency] private readonly IEntityManager _entManager = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly MetaDataSystem _metaSystem = default!;
+    [Dependency] private readonly ChatSystem _chatSystem = default!;
+    [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
 
-//     public override void Execute(IConsoleShell shell, string argStr, string[] args)
-//     {
-//         if (args.Length != 2)
-//         {
-//             shell.WriteLine(Help);
-//             return;
-//         }
+    public override string Command => "echo_chat";
 
-//         var name = args[1];
-//         if (name.Length > IdCardConsoleComponent.MaxFullNameLength)
-//         {
-//             shell.WriteLine(Loc.GetString("cmd-rename-too-long"));
-//             return;
-//         }
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length != 3)
+        {
+            shell.WriteLine(Help);
+            return;
+        }
 
-//         if (!TryParseUid(args[0], shell, _entManager, out var entityUid))
-//             return;
+        if (!TryParseUid(args[0], shell, _entManager, out var entityUid))
+            return;
 
-//         _metaSystem.SetEntityName(entityUid.Value, name);
-//     }
+        var massaged = args[1];
 
-//     private bool TryParseUid(string str, IConsoleShell shell,
-//         IEntityManager entMan, [NotNullWhen(true)] out EntityUid? entityUid)
-//     {
-//         if (NetEntity.TryParse(str, out var entityUidNet) && _entManager.TryGetEntity(entityUidNet, out entityUid) && entMan.EntityExists(entityUid))
-//             return true;
+        if (args[2] == "emote")
+        {
+            _chatSystem.TrySendInGameICMessage(entityUid.Value, massaged, InGameICChatType.Emote, ChatTransmitRange.Normal);
+            shell.WriteLine($"зашли сюда (args[2] == emote)");
+        }
+        else if (args[2] == "speak")
+        {
+            _chatSystem.TrySendInGameICMessage(entityUid.Value, massaged, InGameICChatType.Speak, ChatTransmitRange.Normal);
+            shell.WriteLine($"зашли сюда (args[2] == speak)");
+        }
 
-//         if (_playerManager.TryGetSessionByUsername(str, out var session) && session.AttachedEntity.HasValue)
-//         {
-//             entityUid = session.AttachedEntity.Value;
-//             return true;
-//         }
+        shell.WriteLine($"Инфа о переменных: \nargs[0]: {args[0]} \nargs[1]: {massaged} \nargs[2]: {args[2]}");
+        Log.Information($"Инфа о переменных: args[0]: {args[0]} args[1]: {massaged} args[2]: {args[2]}");
+        Log.Debug($"Инфа о переменных: args[0]: {args[0]} args[1]: {massaged} args[2]: {args[2]}");
 
-//         if (session == null)
-//             shell.WriteError(Loc.GetString("cmd-rename-not-found", ("target", str)));
-//         else
-//             shell.WriteError(Loc.GetString("cmd-rename-no-entity", ("target", str)));
+        //_adminLogger.Add(LogType.Chat, LogImpact.Low, $"Server announcement: {message}");
+    }
 
-//         entityUid = EntityUid.Invalid;
-//         return false;
-//     }
-// }
+    private bool TryParseUid(string str, IConsoleShell shell,
+        IEntityManager entMan, [NotNullWhen(true)] out EntityUid? entityUid)
+    {
+        if (NetEntity.TryParse(str, out var entityUidNet) && _entManager.TryGetEntity(entityUidNet, out entityUid) && entMan.EntityExists(entityUid))
+            return true;
+
+        if (_playerManager.TryGetSessionByUsername(str, out var session) && session.AttachedEntity.HasValue)
+        {
+            entityUid = session.AttachedEntity.Value;
+            return true;
+        }
+
+        if (session == null)
+            shell.WriteError(Loc.GetString("cmd-rename-not-found", ("target", str)));
+        else
+            shell.WriteError(Loc.GetString("cmd-rename-no-entity", ("target", str)));
+
+        entityUid = EntityUid.Invalid;
+        return false;
+    }
+}
