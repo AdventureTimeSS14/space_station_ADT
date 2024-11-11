5672b066f6e56805c2e5bd5bd56aceb40d0b33ec
PyotrIgn <131798882+PyotrIgn@users.noreply.github.com>
2024-11-11 13:35:49 +0400

    перенос Небольшие правки, расширил условие, вынес в функцию

--- Content.Server/ADT/AdditionalMapLoader/AdditionalMapLoaderSystem.cs
+++ Content.Server/ADT/AdditionalMapLoader/AdditionalMapLoaderSystem.cs
@@ -8,7 +8,7 @@ namespace Content.Server.ADT.AdditionalMapLoader;
 public sealed class AdditionalMapLoaderSystem : EntitySystem
 {
     [Dependency] private readonly GameTicking.GameTicker _gameTicker = default!;
-    [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly IMapManager _mapManager = default!;
 
     public override void Initialize()
@@ -21,18 +21,25 @@ public sealed class AdditionalMapLoaderSystem : EntitySystem
     private void OnGetMaps(LoadingMapsEvent args)
     {
         var firstMap = args.Maps[0];
-        if (!_prototype.TryIndex<AdditionalMapPrototype>(firstMap.ID, out var proto))
+        if (!_prototypeManager.TryIndex<AdditionalMapPrototype>(firstMap.ID, out var proto))
             return;
 
         foreach (var mapProtoId in proto.MapProtoIds)
         {
-            if (!_prototype.TryIndex(mapProtoId, out var mapProto))
+            if (!_prototypeManager.TryIndex(mapProtoId, out var mapProto))
+            {
+                Log.Error($"Prototype not found with ID '{mapProtoId.Id}' in '{proto.ID}'. " +
+                $"Please ensure this prototype exists in '- type: additionalMap' check `maps:`!!");
                 continue;
-
-            var map = _mapManager.CreateMap();
-            _mapManager.AddUninitializedMap(map);
-            _gameTicker.LoadGameMap(mapProto, map, null);
-            _mapManager.DoMapInitialize(map);
+            }
+            CreateAndInitializeMap(mapProto);
         }
+    }
+    private void CreateAndInitializeMap(Maps.GameMapPrototype mapProto)
+    {
+        var map = _mapManager.CreateMap();
+        _mapManager.AddUninitializedMap(map);
+        _gameTicker.LoadGameMap(mapProto, map, null);
+        _mapManager.DoMapInitialize(map);
     }
 }
