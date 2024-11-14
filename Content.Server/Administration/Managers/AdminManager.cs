65057f3d0fe5b374b649feb0f3a1104908421119
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-11-15 01:00:10 +0300

    comment

--- Content.Server/Administration/Managers/AdminManager.cs
+++ Content.Server/Administration/Managers/AdminManager.cs
@@ -112,10 +112,10 @@ namespace Content.Server.Administration.Managers
             var playerData = session.ContentData()!;
             playerData.Stealthed = true;
             reg.Data.Stealth = true;
-
-            _chat.DispatchServerMessage(session, Loc.GetString("admin-manager-stealthed-message"));
-            _chat.SendAdminAnnouncement(Loc.GetString("admin-manager-self-de-admin-message", ("exAdminName", session.Name)), AdminFlags.Stealth);
-            _chat.SendAdminAnnouncement(Loc.GetString("admin-manager-self-enable-stealth", ("stealthAdminName", session.Name)), flagWhitelist: AdminFlags.Stealth);
+            // ADT Tweak commented
+            // _chat.DispatchServerMessage(session, Loc.GetString("admin-manager-stealthed-message"));
+            // _chat.SendAdminAnnouncement(Loc.GetString("admin-manager-self-de-admin-message", ("exAdminName", session.Name)), AdminFlags.Stealth);
+            // _chat.SendAdminAnnouncement(Loc.GetString("admin-manager-self-enable-stealth", ("stealthAdminName", session.Name)), flagWhitelist: AdminFlags.Stealth);
         }
 
         public void UnStealth(ICommonSession session)
