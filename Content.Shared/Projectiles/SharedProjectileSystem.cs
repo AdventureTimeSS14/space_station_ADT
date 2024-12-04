047b84b2e7e356b1c166a7d5a921a4502ff13ba3
Inconnu1337 <elasbus@gmail.com>
2024-12-04 23:16:26 +0200

    friendly_fire

--- Content.Shared/Projectiles/SharedProjectileSystem.cs
+++ Content.Shared/Projectiles/SharedProjectileSystem.cs
@@ -183,3 +183,19 @@ public record struct ProjectileReflectAttemptEvent(EntityUid ProjUid, Projectile
 /// </summary>
 [ByRefEvent]
 public record struct ProjectileHitEvent(DamageSpecifier Damage, EntityUid Target, EntityUid? Shooter = null);
+
+// ADT TornadoTech TWEAK START
+public sealed class ProjectileHitAttemptEvent(DamageSpecifier damage, EntityUid target, EntityUid? shooter = null) : CancellableEntityEventArgs
+{
+    public DamageSpecifier Damage { get; } = damage;
+    public EntityUid Target { get; } = target;
+    public EntityUid? Shooter { get; } = shooter;
+}
+
+public sealed class HitScanHitAttemptEvent(EntityUid? shooter, EntityUid target, EntityUid sourceItem) : CancellableEntityEventArgs
+{
+    public readonly EntityUid? Shooter = shooter;
+    public readonly EntityUid Target = target;
+    public readonly EntityUid SourceItem = sourceItem;
+}
+// ADT TornadoTech TWEAK END
