01802ddc3cb131050bf4b193761e0e8a92968e9f
eddiedoesGIT <Stepanyan7803@gmail.com>
2024-12-13 00:56:58 +0300

    Оказывается, такой комопнент был. Фикс.

--- Content.Server/ADT/Misc/NukeopsShuttleComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server.ADT.Misc;
-/// <summary>
-/// Added to Nukeops shuttle. Used for drone control.
-/// </summary>
-[RegisterComponent]
-public sealed partial class NukeopsShuttleComponent : Component
-{
-}
