7ba82b43fc319f98db5ca16135e385023015ad1b
RevengenRat <138193222+Ratyyy@users.noreply.github.com>
2024-12-23 21:33:55 +0200

    добавляем лаваленд!!

---
+++ Content.Shared/ADT/Animations/FlipOnHitComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Animations
+{
+[RegisterComponent, NetworkedComponent]
+public sealed partial class FlipOnHitComponent : Component
+{}
+}
+
+
---
+++ Resources/Locale/ru-RU/ADT/shuttles/docking-console.ftl
@@ -0,0 +1,6 @@
+docking-console-no-shuttle = Шаттл не найден
+docking-console-ftl = БСС
+
+mining-console-window-title = Консоль управления шахтёрским шаттлом
+
+shuttle-destination-lavaland = Лаваленд
