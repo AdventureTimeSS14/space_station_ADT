2df7c20190e4f0dc32a034298a38794837762bc7
RevengenRat <138193222+Ratyyy@users.noreply.github.com>
2024-12-28 14:35:43 +0200

    Revert "ну, просто что если"
    This reverts commit 587f66c29c733413736d002e7d852b8294d564e9.

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -98,61 +98,61 @@ public sealed class CargoTest
         await pair.CleanReturnAsync();
     }
 
-    // [Test]
-    // public async Task NoStaticPriceAndStackPrice()
-    // {
-    //     await using var pair = await PoolManager.GetServerClient();
-    //     var server = pair.Server;
+    [Test]
+    public async Task NoStaticPriceAndStackPrice()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
 
-    //     var testMap = await pair.CreateTestMap();
+        var testMap = await pair.CreateTestMap();
 
-    //     var entManager = server.ResolveDependency<IEntityManager>();
-    //     var mapManager = server.ResolveDependency<IMapManager>();
-    //     var protoManager = server.ResolveDependency<IPrototypeManager>();
+        var entManager = server.ResolveDependency<IEntityManager>();
+        var mapManager = server.ResolveDependency<IMapManager>();
+        var protoManager = server.ResolveDependency<IPrototypeManager>();
 
-    //     await server.WaitAssertion(() =>
-    //     {
-    //         var mapId = testMap.MapId;
-    //         var grid = mapManager.CreateGridEntity(mapId);
-    //         var coord = new EntityCoordinates(grid.Owner, 0, 0);
+        await server.WaitAssertion(() =>
+        {
+            var mapId = testMap.MapId;
+            var grid = mapManager.CreateGridEntity(mapId);
+            var coord = new EntityCoordinates(grid.Owner, 0, 0);
 
-    //         var protoIds = protoManager.EnumeratePrototypes<EntityPrototype>()
-    //             .Where(p => !p.Abstract)
-    //             .Where(p => !pair.IsTestPrototype(p))
-    //             .Where(p => !p.Components.ContainsKey("MapGrid")) // Grids are not for sale.
-    //             .Select(p => p.ID)
-    //             .ToList();
+            var protoIds = protoManager.EnumeratePrototypes<EntityPrototype>()
+                .Where(p => !p.Abstract)
+                .Where(p => !pair.IsTestPrototype(p))
+                .Where(p => !p.Components.ContainsKey("MapGrid")) // Grids are not for sale.
+                .Select(p => p.ID)
+                .ToList();
 
-    //         foreach (var proto in protoIds)
-    //         {
-    //             var ent = entManager.SpawnEntity(proto, coord);
+            foreach (var proto in protoIds)
+            {
+                var ent = entManager.SpawnEntity(proto, coord);
 
-    //             if (entManager.TryGetComponent<StackPriceComponent>(ent, out var stackpricecomp)
-    //                 && stackpricecomp.Price > 0)
-    //             {
-    //                 if (entManager.TryGetComponent<StaticPriceComponent>(ent, out var staticpricecomp))
-    //                 {
-    //                     Assert.That(staticpricecomp.Price, Is.EqualTo(0),
-    //                         $"The prototype {proto} has a StackPriceComponent and StaticPriceComponent whose values are not compatible with each other.");
-    //                 }
-    //             }
+                if (entManager.TryGetComponent<StackPriceComponent>(ent, out var stackpricecomp)
+                    && stackpricecomp.Price > 0)
+                {
+                    if (entManager.TryGetComponent<StaticPriceComponent>(ent, out var staticpricecomp))
+                    {
+                        Assert.That(staticpricecomp.Price, Is.EqualTo(0),
+                            $"The prototype {proto} has a StackPriceComponent and StaticPriceComponent whose values are not compatible with each other.");
+                    }
+                }
 
-    //             if (entManager.HasComponent<StackComponent>(ent))
-    //             {
-    //                 if (entManager.TryGetComponent<StaticPriceComponent>(ent, out var staticpricecomp))
-    //                 {
-    //                     Assert.That(staticpricecomp.Price, Is.EqualTo(0),
-    //                         $"The prototype {proto} has a StackComponent and StaticPriceComponent whose values are not compatible with each other.");
-    //                 }
-    //             }
+                if (entManager.HasComponent<StackComponent>(ent))
+                {
+                    if (entManager.TryGetComponent<StaticPriceComponent>(ent, out var staticpricecomp))
+                    {
+                        Assert.That(staticpricecomp.Price, Is.EqualTo(0),
+                            $"The prototype {proto} has a StackComponent and StaticPriceComponent whose values are not compatible with each other.");
+                    }
+                }
 
-    //             entManager.DeleteEntity(ent);
-    //         }
-    //         mapManager.DeleteMap(mapId);
-    //     });
+                entManager.DeleteEntity(ent);
+            }
+            mapManager.DeleteMap(mapId);
+        });
 
-    //     await pair.CleanReturnAsync();
-    // }
+        await pair.CleanReturnAsync();
+    }
 
     /// <summary>
     /// Tests to see if any items that are valid for cargo bounties can be sliced into items that
