ae0160d4675ab21ab9640dc1b3a84cbbd61f929d
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2024-12-30 04:02:14 +0300

    Revert "Уведомления о обновлениях сервера через Discord webhook (#920)"
    This reverts commit 34938461fef277353721da9926cf7b6d0907aa73.

--- Content.Shared/ADT/CCVar/CCVars.WebhookDiscord.cs
+++ Content.Shared/ADT/CCVar/CCVars.WebhookDiscord.cs
@@ -10,19 +10,5 @@ public sealed class ADTDiscordWebhookCCVars : CVars
     /// URL of the Discord webhook which will relay adminwho info to the channel.
     /// </summary>
     public static readonly CVarDef<string> DiscordAdminwhoWebhook =
-        CVarDef.Create("discord.adminwho_webhook", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL | CVar.ARCHIVE);
-
-    /// <summary>
-    /// This constant specifies a webhook that will send a message to Discord when a server updates.
-    /// </summary>
-    public static readonly CVarDef<string> DiscordServerUpdateWebhook =
-        CVarDef.Create("discord.server_update_webhook", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL | CVar.ARCHIVE);
-
-    /// <summary>
-    /// This constant specifies whether a ping should be sent to a specific Discord role
-    /// when the server update notification is triggered. If set to <c>true</c>, a ping will be sent to the role.
-    /// If set to <c>false</c>, no ping will be sent.
-    /// </summary>
-    public static readonly CVarDef<bool> ShouldPingOnUpdate =
-        CVarDef.Create("discord.server_update_webhook_ping", true, CVar.SERVERONLY | CVar.ARCHIVE);
+        CVarDef.Create("discord.adminwho_webhook", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
 }
