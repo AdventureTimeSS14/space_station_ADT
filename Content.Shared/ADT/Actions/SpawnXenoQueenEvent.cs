5960127f74c5470086c444d7459408fc8f6b17b6
NameLunar <dimasikgamer228@gmail.com>
2025-01-08 00:04:26 +0700

    Добавли DataDefinition

--- Content.Shared/ADT/Actions/SpawnXenoQueenEvent.cs
+++ Content.Shared/ADT/Actions/SpawnXenoQueenEvent.cs
@@ -1,11 +1,11 @@
 using Content.Shared.Actions;
 using Content.Shared.Storage;
-using Content.Shared.Magic;
 
 namespace Content.Shared.ADT.Events;
 
 // TODO: This class needs combining with InstantSpawnSpellEvent
-public sealed partial class SpawnXenoQueenEvent : WorldTargetActionEvent, ISpeakSpell 
+[DataDefinition]
+public sealed partial class SpawnXenoQueenEvent : WorldTargetActionEvent
 {
     [DataField]
     public List<EntitySpawnEntry> Prototypes = new();
