f85842c71e3324322a90ea0fcc0bdde14d30c0a1
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-01-07 23:03:02 +0300

    ww

--- Content.Server/Administration/ServerApi.cs
+++ Content.Server/Administration/ServerApi.cs
@@ -781,6 +781,7 @@ public sealed partial class ServerApi : IPostInjectInit
         }
         // ICommonSession? session;
         var username = new NetUserId(new Guid($"{actor.Guid}"));
+        var username = new NetUserId({actor.Guid});
 
         if (!_player.TryGetSessionById(username, out var session))
         {
