93bff56a8af1a1bcb8601638463fae6a54ac66b3
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-01-08 22:36:47 +0300

    fix session custom

--- Content.Server/Administration/ServerApi.cs
+++ Content.Server/Administration/ServerApi.cs
@@ -781,10 +781,7 @@ public sealed partial class ServerApi : IPostInjectInit
         }
         // ICommonSession? session;
         var username = new NetUserId(new Guid($"{actor.Guid}"));
-
-
-        var sessionCustom = _playerManager.GetSessionById(username);
-
+        var sessionCustom = _playerManager.CreateAndAddSession(username, actor.Name)
 
         _sawmill.Info($"Session found: {sessionCustom}");
 
