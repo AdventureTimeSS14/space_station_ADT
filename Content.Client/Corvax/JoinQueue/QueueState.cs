5b817d5b18c1e6c20b9287cfa69ec5bb06fe89d5
Inconnu <177014427+Inconnu1337@users.noreply.github.com>
2025-01-31 20:05:20 +0200

    Update QueueState.cs

--- Content.Client/Corvax/JoinQueue/QueueState.cs
+++ Content.Client/Corvax/JoinQueue/QueueState.cs
@@ -3,10 +3,10 @@ using Robust.Client.Console;
 using Robust.Client.GameObjects;
 using Robust.Client.State;
 using Robust.Client.UserInterface;
-using Robust.Shared.Audio;
-using Robust.Client.Audio;
-using Robust.Shared.Audio.Systems;
 using Robust.Shared.Player;
+using Robust.Client.Audio;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
 
 namespace Content.Client.Corvax.JoinQueue;
 
@@ -14,9 +14,9 @@ public sealed class QueueState : State
 {
     [Dependency] private readonly IUserInterfaceManager _userInterfaceManager = default!;
     [Dependency] private readonly IClientConsoleHost _consoleHost = default!;
-    [Dependency] private readonly AudioSystem _audio = default!;
+    //[Dependency] private readonly SharedAudioSystem _audio = default!;
 
-    private const string JoinSoundPath = "/Audio/Effects/voteding.ogg";
+    //private const string JoinSoundPath = "/Audio/Effects/voteding.ogg";
 
     private QueueGui? _gui;
 
@@ -33,13 +33,13 @@ public sealed class QueueState : State
         _gui!.QuitPressed -= OnQuitPressed;
         _gui.Dispose();
 
-        Ding();
+        //Ding();
     }
 
-    private void Ding()
-    {
-        _audio.PlayGlobal(JoinSoundPath, Filter.Local(), false);
-    }
+    //private void Ding()
+    //{
+    //    _audio.PlayGlobal(JoinSoundPath, Filter.Local(), false);
+    //}
 
     public void OnQueueUpdate(MsgQueueUpdate msg)
     {
