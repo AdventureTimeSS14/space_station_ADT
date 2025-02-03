69cbaff5a2ea97c73c2671bcc193281ea7e99e90
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-02-03 12:03:25 +0000

    [ADMIN] Add altVerbs admin rooms (#1052)
    ## Описание PR
    Внесены некоторые изменения в официальный код, добавляющие
    альтернативные вербы (altVerbs) для администраторов. Теперь вместо одной
    административной комнаты (арумы) доступно сразу пять различных комнат,
    каждая с уникальным назначением и функционалом.
    
    ## Почему / Баланс
    Не затрагивает баланс игры для обычных игроков, так как новые функции
    предназначены исключительно для административного использования.
    
    **Ссылка на публикацию в Discord**
    
    
    ## Техническая информация
    Добавлены новые вербы, позволяющие администраторам перемещаться в пять
    различных административных комнат. Каждая комната имеет собственный
    идентификатор, иконку и описание, что облегчает их использование и
    навигацию.
    
    Все объявленные прототипы `- type: adminArenaVerb*. Собираются и
    добавляются в альт верб админа
    
    - Плюс была сортировка using в прошлом коде.
    
    ## Медиа
    
    ### Новый альт верб
    
    ![image](https://github.com/user-attachments/assets/e0308b0f-c559-4520-9113-e8b57c6c8891)
    
    ### Обычная арума (classic)
    
    ![image](https://github.com/user-attachments/assets/ee72f089-98e8-4f34-b924-9ed43d4117b6)
    
    ### Dev
    
    ![image](https://github.com/user-attachments/assets/6a8459b0-1042-4f07-821b-9cb3502f97fa)
    
    ### Hell
    
    ![image](https://github.com/user-attachments/assets/d66a3d16-da63-4c2f-be24-608ed652591b)
    
    ### Paradise
    
    ![image](https://github.com/user-attachments/assets/e09c5ad3-2bf7-49a4-9924-7312d6a4050b)
    
    ### SandBox
    
    ![image](https://github.com/user-attachments/assets/03d8cc51-6852-4d1e-bf76-d917e1453fa4)
    
    ### Именования
    
    ![image](https://github.com/user-attachments/assets/e334de36-0c8a-4f65-a7b7-c0c71ee4cce0)
    
    ![image](https://github.com/user-attachments/assets/0bcdfff4-f146-4e0e-bdca-11922c6fb6a1)
    
    
    
    
    ## Требования
    - [X] Я прочитал(а) и следую [Руководству по созданию пулл
    реквестов](https://docs.spacestation14.com/en/general-development/codebase-info/pull-request-guidelines.html).
    - [X] Данный пулл реквест не требует демонстрации в игре.
    
    ## Критические изменения
    Нет критических изменений, влияющих на существующий код или
    функциональность.
    
    @NameLunar Спасибо большое за карты
    # ❤️
    
    **Чейнджлог**
    :cl: Шрёдька
    - tweak: Добавлены альтернативные админ вербы для перемещения в пять
    различных админ рум.

--- Content.Server/ADT/Administration/Systems/AdminTimeOperSpawnVerbs.cs
+++ Content.Server/ADT/Administration/Systems/AdminTimeOperSpawnVerbs.cs
@@ -1,33 +1,41 @@
+using Content.Shared.Actions;
+using Content.Shared.Administration;
+using Content.Shared.ComponentalActions.Components;
 using Content.Shared.Database;
 using Content.Shared.Roles;
 using Content.Shared.Verbs;
-using Robust.Shared.Player;
-using Robust.Shared.Utility;
-using Robust.Server.GameObjects;
 using Robust.Shared.Audio.Systems;
-using Content.Shared.ComponentalActions.Components;
-using Content.Shared.Actions;
-using Content.Server.Administration.Managers;
+using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
-// ADT Content: Time Patrol "ОБВА" by Schrodinger71
+
+// ADT Content: Time Patrol "ОБВА" by 🐾 Schrödinger's Code 🐾
+/*
+    ╔════════════════════════════════════╗
+    ║   Schrödinger's Cat Code   🐾      ║
+    ║   /\_/\\                           ║
+    ║  ( o.o )  Meow!                    ║
+    ║   > ^ <                            ║
+    ╚════════════════════════════════════╝
+
+*/
 namespace Content.Server.Administration.Systems;
 
 public sealed partial class AdminVerbSystem
 {
     [Dependency] private readonly SharedAudioSystem _audio = default!;
-    [Dependency] private readonly TransformSystem _transform = default!;
-    [Dependency] private readonly IAdminManager _admins = default!;
 
-
-    // All antag verbs have names so invokeverb works.
     private void AddAdminTimeOperSpawnVerbs(GetVerbsEvent<Verb> args)
     {
         if (!EntityManager.TryGetComponent(args.User, out ActorComponent? actor))
             return;
 
         var player = actor.PlayerSession;
-        //var hasBan = _admins.HasAdminFlag(player, AdminFlags.Ban);
+
+        if (!_adminManager.HasAdminFlag(player, AdminFlags.Admin))
+            return;
+
         if (_adminManager.IsAdmin(player))
         {
             if (TryComp(args.Target, out ActorComponent? targetActor))
@@ -47,7 +55,7 @@ public sealed partial class AdminVerbSystem
                         }
 
                         var stationUid = _stations.GetOwningStation(args.Target);
-                        ProtoId<JobPrototype> job =  "ADTJobTimePatrol";
+                        ProtoId<JobPrototype> job = "ADTJobTimePatrol";
                         var profile = _ticker.GetPlayerProfile(targetActor.PlayerSession);
                         var mobUid = _spawning.SpawnPlayerMob(coords.Value, job, profile, stationUid);
                         var targetMind = _mindSystem.GetMind(args.Target);
