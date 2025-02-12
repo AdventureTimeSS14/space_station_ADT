1bafa443bd4c5101fc51fa2dcac668aea12dd886
FaDeOkno <logkedr18@gmail.com>
2025-02-12 18:35:16 +0400

    add: Добавлена поддержка разных сложностей и погоды, + создан модификатор "ветер" для экспедиций

---
+++ Content.Shared/ADT/Salvage/Expeditions/Modifiers/SalvageWindMod.cs
@@ -0,0 +1,29 @@
+using Content.Shared.Salvage.Expeditions.Modifiers;
+using Content.Shared.Weather;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
+
+namespace Content.Shared.ADT.Salvage.Expeditions.Modifiers;
+
+[Prototype("salvageWindMod")]
+public sealed partial class SalvageWindMod : IPrototype, IBiomeSpecificMod
+{
+    [IdDataField] public string ID { get; } = default!;
+
+    [DataField("desc")] public LocId Description { get; private set; } = string.Empty;
+
+    /// <inheritdoc/>
+    [DataField("cost")]
+    public float Cost { get; private set; } = 0f;
+
+    /// <inheritdoc/>
+    [DataField("biomes", customTypeSerializer: typeof(PrototypeIdListSerializer<SalvageBiomeModPrototype>))]
+    public List<string>? Biomes { get; private set; } = null;
+
+    /// <summary>
+    /// Wind strength
+    /// </summary>
+    [DataField("weather")]
+    public float Strength = 0f;
+}
