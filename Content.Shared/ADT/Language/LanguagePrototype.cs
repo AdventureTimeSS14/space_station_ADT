3fbb20ff30bbe9943be4451aaf599f5e26a3e34a
FaDeOkno <logkedr18@gmail.com>
2025-02-27 17:01:15 +0400

    add(src): Добавление `ILanguageCondition[]` в прототип языка, а так же парентинг языков

--- Content.Shared/ADT/Language/LanguagePrototype.cs
+++ Content.Shared/ADT/Language/LanguagePrototype.cs
@@ -1,12 +1,23 @@
+using System.Linq;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
 
 namespace Content.Shared.ADT.Language;
 
 [Prototype("language")]
-public sealed class LanguagePrototype : IPrototype
+public sealed class LanguagePrototype : IPrototype, IInheritingPrototype
 {
     [IdDataField]
     public string ID { get; private set; } = default!;
+
+    /// <inheritdoc />
+    [ParentDataField(typeof(AbstractPrototypeIdArraySerializer<LanguagePrototype>))]
+    public string[]? Parents { get; }
+
+    /// <inheritdoc />
+    [NeverPushInheritance]
+    [AbstractDataField]
+    public bool Abstract { get; }
 
     [DataField]
     public int Priority = 1;
@@ -16,6 +27,9 @@ public sealed class LanguagePrototype : IPrototype
 
     [DataField]
     public bool ShowUnderstood = true;
+
+    [DataField]
+    public bool Vocal = true;
 
     [DataField]
     public Color? UiColor;
@@ -32,6 +46,23 @@ public sealed class LanguagePrototype : IPrototype
 
     [DataField("speech", required: true, serverOnly: true)]
     private ILanguageType _languageType = null!;
+
+    public ILanguageCondition[] Conditions
+    {
+        get
+        {
+            foreach (var item in _conditions)
+            {
+                item.Language = ID;
+            }
+
+            return _conditions;
+        }
+        set => _conditions = value;
+    }
+
+    [DataField("conditions", serverOnly: true)]
+    private ILanguageCondition[] _conditions = Array.Empty<ILanguageCondition>();
 
     public string LocalizedName => Loc.GetString("language-" + ID + "-name");
     public string LocalizedDescription => Loc.GetString("language-" + ID + "-description");
