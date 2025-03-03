184f2f2294adbd1c3a1557ede7aac12fecd6ab01
Darki255 <Kurachiy75@gmail.com>
2025-03-03 20:03:20 +0300

    Добавлен .cs компонент. Перенес список в компонент

--- Content.Server/ADT/Radiation/RadiationPopupSystems.cs
+++ Content.Server/ADT/Radiation/RadiationPopupSystems.cs
@@ -1,3 +1,4 @@
+using Content.Server.ADT.Radiation;
 using Content.Shared.Popups;
 using Content.Shared.Radiation.Events;
 using Robust.Shared.Player;
@@ -18,6 +19,9 @@ public sealed class RadiationNoticingSystem : EntitySystem
 
     private void OnIrradiated(EntityUid uid, ActorComponent actorComponent, OnIrradiatedEvent args)
     {
+        if (!HasComp<RadiationPopupComponent>(uid))
+            return;
+
         // Roll chance for popup messages
         // This is per radiation update tick,
         // is tuned so that when being irradiated with 1 rad/sec, see 1 message every 5-ish seconds on average
@@ -31,21 +35,10 @@ public sealed class RadiationNoticingSystem : EntitySystem
 
     private void SendRadiationPopup(EntityUid uid)
     {
-        List<string> msgArr = [
-                "radiation-noticing-message-0",
-                "radiation-noticing-message-1",
-                "radiation-noticing-message-2",
-                "radiation-noticing-message-3",
-                "radiation-noticing-message-4",
-                "radiation-noticing-message-5",
-                "radiation-noticing-message-6",
-                "radiation-noticing-message-7"
-            ];
-
         // Todo: detect possessing specific types of organs/blood/etc and conditionally add related messages to the list
 
         // pick a random message
-        var msgId = _random.Pick(msgArr);
+        var msgId = _random.Pick(RadiationPopupComponent.msgArr);
         var msg = Loc.GetString(msgId);
 
         // show it as a popup
