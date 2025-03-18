90fbcf9290907945150fb6455c39a58276a2e8bf
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-03-18 14:12:37 +0300

    init

---
+++ Content.Shared/ADT/Roles/JobRequirement/SponsorTierRequirement.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+using Content.Shared.Localizations;
+using Content.Shared.Players.PlayTimeTracking;
+using Content.Shared.Preferences;
+using Content.Shared.Roles.Jobs;
+using JetBrains.Annotations;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Roles;
+
+[UsedImplicitly]
+[Serializable, NetSerializable]
+public sealed partial class SponsorTierRequirement : JobRequirement
+{
+
+}
