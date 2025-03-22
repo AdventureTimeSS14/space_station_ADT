8c725f59b58d66a3fb8698b506e717d08603192a
Darki255 <Kurachiy75@gmail.com>
2025-03-22 20:04:43 +0300

    Повторный фикс

--- Content.Shared/Weapons/Melee/Components/MeleeThrowOnHitComponent.cs
+++ Content.Shared/Weapons/Melee/Components/MeleeThrowOnHitComponent.cs
@@ -35,22 +35,6 @@ public sealed partial class MeleeThrowOnHitComponent : Component
     /// <summary>
     /// How long should this stun the target, if applicable?
     /// </summary>
-<<<<<<< HEAD
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    [AutoNetworkedField]
-    public bool Enabled = true;
-
-    // ADT tweak start
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public DamageSpecifier? CollideDamage;
-
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public DamageSpecifier? ToCollideDamage;
-
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public bool DownOnHit = false;
-    // ADT tweak end
-=======
     [DataField, AutoNetworkedField]
     public TimeSpan? StunTime;
 
@@ -59,70 +43,13 @@ public sealed partial class MeleeThrowOnHitComponent : Component
     /// </summary>
     [DataField, AutoNetworkedField]
     public bool ActivateOnThrown;
->>>>>>> e8c13fe325c5de84c2ec31ac5c70f254cf9333f3
 }
 
 /// <summary>
 /// Raised a weapon entity with <see cref="MeleeThrowOnHitComponent"/> to see if a throw is allowed.
 /// </summary>
-<<<<<<< HEAD
-[RegisterComponent, NetworkedComponent]
-[AutoGenerateComponentState]
-[Access(typeof(MeleeThrowOnHitSystem))]
-public sealed partial class MeleeThrownComponent : Component
-{
-    /// <summary>
-    /// The velocity of the throw
-    /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    [AutoNetworkedField]
-    public Vector2 Velocity;
-
-    /// <summary>
-    /// How long the throw will last.
-    /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    [AutoNetworkedField]
-    public float Lifetime;
-
-    /// <summary>
-    /// How long we wait to start accepting collision.
-    /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public float MinLifetime;
-
-    /// <summary>
-    /// At what point in time will the throw be complete?
-    /// </summary>
-    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
-    [AutoNetworkedField]
-    public TimeSpan ThrownEndTime;
-
-    /// <summary>
-    /// At what point in time will the <see cref="MinLifetime"/> be exhausted
-    /// </summary>
-    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
-    [AutoNetworkedField]
-    public TimeSpan MinLifetimeTime;
-
-    /// <summary>
-    /// the status to which the entity will return when the thrown ends
-    /// </summary>
-    [DataField]
-    public BodyStatus PreviousStatus;
-
-    // ADT tweak start
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public DamageSpecifier? CollideDamage;
-
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public DamageSpecifier? ToCollideDamage;
-    // ADT tweak end
-}
-=======
 [ByRefEvent]
 public record struct AttemptMeleeThrowOnHitEvent(EntityUid Target, EntityUid? User, bool Cancelled = false, bool Handled = false);
->>>>>>> e8c13fe325c5de84c2ec31ac5c70f254cf9333f3
 
 /// <summary>
 /// Raised a target entity before it is thrown by <see cref="MeleeThrowOnHitComponent"/>.
