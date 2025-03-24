3ab65afef8d828fb401dc3cc04a91cae32824ec0
KashRas2 <140270505+KashRas2@users.noreply.github.com>
2025-03-24 14:50:21 +0300

    ???

--- Content.Server/Speech/ListenEvent.cs
+++ Content.Server/Speech/ListenEvent.cs
@@ -1,12 +1,16 @@
+using Content.Shared.ADT.Language;
+
 namespace Content.Server.Speech;
 
 public sealed class ListenEvent : EntityEventArgs
 {
+    public readonly LanguagePrototype? Language; // ADT-Language
     public readonly string Message;
     public readonly EntityUid Source;
 
-    public ListenEvent(string message, EntityUid source)
+    public ListenEvent(string message, EntityUid source, LanguagePrototype? language = null)) // ADT-Language
     {
+        Language = language; // ADT-Language
         Message = message;
         Source = source;
     }
