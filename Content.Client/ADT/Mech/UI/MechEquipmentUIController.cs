e881f3d1270d93339f48137f80e8eb1c0bc792a7
FaDeOkno <143940725+FaDeOkno@users.noreply.github.com>
2025-03-26 01:58:43 +0400

    Мини qol мехов (#1272)
    ## Описание PR
    <!-- Что вы изменили в этом пулл-реквесте? -->
    Починил самый бесячий баг мехов - сломанное меню
    Позволил мехам крутиться
    
    ## Почему / Баланс
    <!-- Почему оно было изменено и как изменение повлияет на игру и её
    баланс.
    Ссылка на заказ, предложение или баг-репорт
    - [Баг-репорт/Заказ/Предложение](ссылка)-->
    - древний баг
    - древняя предложка
    
    ## Техническая информация
    <!-- Если речь идет об изменении кода, кратко изложите на высоком уровне
    принцип работы нового кода. Перечислите все критические изменения,
    включая изменения пространства имён, публичных классов/методов/полей-
    -->
    Перенёс регистрацию ивентов из контроллера в систему, и удивительным
    образом всё заработало
    Остальное слишком просто чтоб описывать тут
    
    ## Медиа
    <!--Вставьте медиа, демонстрирующее изменения, если это требуется-->
    
    ## Чейнджлог
    <!--
    :cl: (Автор изменения)
    - add: Добавлено пипи
    - remove: Удалено пупу
    - tweak: Изменена унга
    - fix: Исправлена бунга
    -->
    :cl: Котя
    - fix: Меню мехов больше не багается (вроде)
    - add: Мехи научились крутиться в комбат моде

--- Content.Client/ADT/Mech/UI/MechEquipmentUIController.cs
+++ Content.Client/ADT/Mech/UI/MechEquipmentUIController.cs
@@ -12,49 +12,10 @@ public sealed class MechEquipmentUIController : UIController
 {
     [Dependency] private readonly IEntityManager _entityManager = default!;
     [Dependency] private readonly ISharedPlayerManager _playerManager = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
 
     private MechEquipmentMenu? _menu;
 
-    public override void Initialize()
-    {
-
-        EntityManager.EventBus.SubscribeLocalEvent<MechComponent, MechToggleEquipmentEvent>(OnToggleEquipmentAction);
-        EntityManager.EventBus.SubscribeLocalEvent<MechComponent, CloseMechMenuEvent>(OnMechExit);
-
-    }
-
-    private void OnToggleEquipmentAction(EntityUid uid, MechComponent component, MechToggleEquipmentEvent args)
-    {
-        if (!_entityManager.TryGetComponent<MechPilotComponent>(_playerManager.LocalEntity, out var pilot) || pilot.Mech != uid)
-            return;
-        if (args.Handled)
-            return;
-        args.Handled = true;
-        if (!_timing.IsFirstTimePredicted)
-            return;
-
-        List<NetEntity> list = new();
-        foreach (var item in component.EquipmentContainer.ContainedEntities)
-        {
-            list.Add(_entityManager.GetNetEntity(item));
-        }
-        var ev = new PopulateMechEquipmentMenuEvent(list);
-        ToggleMenu(ev);
-    }
-
-    private void OnMechExit(EntityUid uid, MechComponent component, CloseMechMenuEvent args)
-    {
-        if (_menu != null)
-            ToggleMenu(new(new())); // Для закрытия всё равно не сильно нужна эта информация
-    }
-
-    private void OnPopulateRequest(PopulateMechEquipmentMenuEvent ev)
-    {
-        if (_menu != null)
-            _menu.Populate(new PopulateMechEquipmentMenuEvent(ev.Equipment));
-    }
-    private void ToggleMenu(PopulateMechEquipmentMenuEvent ev)
+    public void ToggleMenu()
     {
         if (_menu == null)
         {
@@ -63,7 +24,6 @@ public sealed class MechEquipmentUIController : UIController
             _menu.OnClose += OnWindowClosed;
             _menu.OnOpen += OnWindowOpen;
             _menu.OnSelectEquip += OnSelectEquip;
-            _menu.Populate(ev);
 
             _menu.OpenCentered();
         }
@@ -72,10 +32,14 @@ public sealed class MechEquipmentUIController : UIController
             _menu.OnClose -= OnWindowClosed;
             _menu.OnOpen -= OnWindowOpen;
             _menu.OnSelectEquip -= OnSelectEquip;
-            _menu.Equipment.Clear();
 
             CloseMenu();
         }
+    }
+
+    public void PopulateMenu(List<NetEntity> equip)
+    {
+        _menu?.Populate(equip);
     }
 
     private void OnWindowClosed()
@@ -87,12 +51,12 @@ public sealed class MechEquipmentUIController : UIController
     {
     }
 
-    private void CloseMenu()
+    public void CloseMenu()
     {
         if (_menu == null)
             return;
 
-        _menu.Dispose();
+        _menu.Close();
         _menu = null;
     }
 
