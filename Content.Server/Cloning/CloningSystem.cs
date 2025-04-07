fba4a30195c00697bd88f1b493f9be5ced7a2d27
JackTalentedCoder <159805128+JackTalentedCoder@users.noreply.github.com>
2025-04-08 00:23:14 +0300

    Добавление фишек для воксов

--- Content.Server/Cloning/CloningSystem.cs
+++ Content.Server/Cloning/CloningSystem.cs
@@ -33,6 +33,7 @@ using Robust.Shared.Containers;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
+using Content.Server.Traits.Assorted;
 
 namespace Content.Server.Cloning
 {
@@ -176,6 +177,16 @@ namespace Content.Server.Cloning
 
             if (_configManager.GetCVar(CCVars.BiomassEasyMode))
                 cloningCost = (int) Math.Round(cloningCost * EasyModeCloningCost);
+
+            // Check if they have the uncloneable trait
+            if (TryComp<UncloneableComponent>(bodyToClone, out _))
+            {
+                if (clonePod.ConnectedConsole != null)
+                    _chatSystem.TrySendInGameICMessage(clonePod.ConnectedConsole.Value,
+                        Loc.GetString("cloning-console-uncloneable-trait-error"),
+                        InGameICChatType.Speak, false);
+                return false;
+            }
 
             // biomass checks
             var biomassAmount = _material.GetMaterialAmount(uid, clonePod.RequiredMaterial);
