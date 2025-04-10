3099ca931a2148892f8a6a99a6e4f145f1fc7f7c
RevengenRat <138193222+Ratyyy@users.noreply.github.com>
2025-04-10 17:51:19 +0300

    фикс модуля защиты от эми

--- Content.Server/ADT/Emp/EmpProtactionContainerComponent.cs
+++ Content.Server/ADT/Emp/EmpProtactionContainerComponent.cs
@@ -3,4 +3,7 @@ namespace Content.Server.ADT.EMPProtaction;
 [RegisterComponent]
 public sealed partial class EmpContainerProtactionComponent : Component
 {
+    public EntityUid? BatteryUid;
+    [DataField]
+    public string ContainerId = "cell_slot";
 }
