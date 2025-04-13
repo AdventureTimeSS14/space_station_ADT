8a55a1046d65a8f646be5c39c422617d8bc299b6
Darki255 <Kurachiy75@gmail.com>
2025-04-13 11:15:36 +0300

    init

---
+++ Content.Server/ADT/Administration/Systems/AdminGodSpawnVerbs.cs
@@ -0,0 +1,64 @@
+using Content.Shared.Actions;
+using Content.Shared.Administration;
+using Content.Shared.ComponentalActions.Components;
+using Content.Shared.Database;
+using Content.Shared.Roles;
+using Content.Shared.Verbs;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Server.Administration.Systems;
+
+public sealed partial class AdminVerbSystem
+{
+    private void AddAdminGodrSpawnVerbs(GetVerbsEvent<Verb> args)
+    {
+        if (!EntityManager.TryGetComponent(args.User, out ActorComponent? actorcomponent))
+            return;
+
+        var playersession = actorcomponent.PlayerSession;
+
+        if (!_adminManager.HasAdminFlag(playersession, AdminFlags.Admin))
+            return;
+
+        if (_adminManager.IsAdmin(playersession))
+        {
+            if (TryComp(args.Target, out ActorComponent? targetActor))
+            {
+                args.Verbs.Add(new Verb()
+                {
+                    Text = Loc.GetString("admin-player-actions-god-shape"),
+                    Category = VerbCategory.Admin,
+                    Icon = new SpriteSpecifier.Texture(new("/Textures/Interface/VerbIcons/light.svg.192dpi.png")),
+                    Act = () =>
+                    {
+                        if (!_transformSystem.TryGetMapOrGridCoordinates(args.Target, out var coords))
+                        {
+                            _popup.PopupEntity(Loc.GetString("admin-player-spawn-failed"), args.User, args.User);
+                            return;
+                        }
+
+                        var stationUid = _stations.GetOwningStation(args.Target);
+                        ProtoId<JobPrototype> job = "ADTJobTimeGod"; // Используется как затычка для метода SpawnPlayerMob()
+                        var profile = _ticker.GetPlayerProfile(targetActor.PlayerSession);
+                        var mobUidSpawn = _spawning.SpawnPlayerMob(coords.Value, job, profile, stationUid);
+                        var targetMind = _mindSystem.GetMind(args.Target);
+
+                        _audio.PlayPvs("/Audio/Effects/holy.ogg", mobUidSpawn);
+
+                        EnsureComp<ElectrionPulseActComponent>(mobUidSpawn);
+
+                        if (targetMind != null)
+                        {
+                            _mindSystem.TransferTo(targetMind.Value, mobUidSpawn, true);
+                        }
+                    },
+                    ConfirmationPopup = true,
+                    Impact = LogImpact.High,
+                });
+            }
+        }
+    }
+}
