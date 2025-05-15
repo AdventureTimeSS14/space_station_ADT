b1e4049472ec8a402da862bce4313fa888daa1cd
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-05-15 03:02:35 +0300

    wip(src): попытки налаживания связи клиента и сервера

--- Content.Server/ADT/Discord/ServerDiscordLinkSystem.cs
+++ Content.Server/ADT/Discord/ServerDiscordLinkSystem.cs
@@ -1,31 +1,37 @@
 // using Content.Shared.ADT.Discord;
 // using Robust.Shared.Network;
 // using Content.Server.Database;
+// using Robust.Server.Player;
 
-// namespace Content.Server.ADT.Discord
+// namespace Content.Server.ADT.Discord;
+
+// public sealed class ServerDiscordLinkSystem : EntitySystem
 // {
-//     public sealed class ServerDiscordLinkSystem : EntitySystem
+//     [Dependency] private readonly IServerNetManager _net = default!;
+//     [Dependency] private readonly IServerDbManager _db = default!;
+//     [Dependency] private readonly IPlayerManager _player = default!;
+
+//     public override void Initialize()
 //     {
-//         [Dependency] private readonly IServerNetManager _net = default!;
-//         [Dependency] private readonly IServerDbManager _db = default!;
+//         base.Initialize();
+//         _net.RegisterNetMessage<MsgRequestDiscordId>(HandleRequest);
+//     }
 
-//         public override void Initialize()
+//     private void HandleRequest(MsgRequestDiscordId msg)
+//     {
+//         var discordId = _db.GetDiscordIdAsync(msg.UserId.UserId).GetAwaiter().GetResult();
+
+//         var response = new MsgReceiveDiscordId
 //         {
-//             base.Initialize();
-//             _net.RegisterNetMessage<MsgRequestDiscordId>(HandleRequest);
-//         }
+//             UserId = msg.UserId,
+//             DiscordId = discordId
+//         };
 
-//         private async void HandleRequest(MsgRequestDiscordId msg, EntitySessionEventArgs args)
+//         var session = _player.GetSessionById(msg.UserId);
+//         if (session != null)
 //         {
-//             var discordId = await _db.GetDiscordIdAsync(msg.UserId.UserId);
-
-//             var response = new MsgReceiveDiscordId
-//             {
-//                 UserId = msg.UserId,
-//                 DiscordId = discordId
-//             };
-
-//             _net.ServerSendMessage(response, args.SenderSession.ConnectedClient);
+//             var channel = session.Channel;
+//             _net.ServerSendMessage(response, channel);
 //         }
 //     }
 // }
--- Content.Shared/ADT/Discord/SharedDiscordLinking.cs
+++ Content.Shared/ADT/Discord/SharedDiscordLinking.cs
@@ -1,45 +1,58 @@
+// using System.IO;
 // using Lidgren.Network;
 // using Robust.Shared.Network;
 // using Robust.Shared.Serialization;
 
-// namespace Content.Shared.ADT.Discord
+// namespace Content.Shared.ADT.Discord;
+
+// /// <summary>
+// /// Клиент -> Сервер: Запросить Discord ID по NetUserId
+// /// </summary>
+// public sealed class MsgRequestDiscordId : NetMessage
 // {
-//     // Запрос на проверку состояния привязки к Discord
-//     public sealed class MsgRequestDiscordId : NetMessage
+//     public override MsgGroups MsgGroup => MsgGroups.Command;
+//     public NetUserId UserId;
+
+//     public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer)
 //     {
-//         public NetUserId UserId { get; set; }
-
-//         public override MsgGroups MsgGroup => MsgGroups.Command;
-
-//         public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer)
-//         {
-//             UserId = buffer.ReadUserId();
-//         }
-
-//         public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer)
-//         {
-//             buffer.Write(UserId);
-//         }
+//         // Считаем все байты из сообщения, обернём в MemoryStream
+//         using var mem = new MemoryStream(buffer.ReadBytes(buffer.LengthBytes));
+//         UserId = serializer.Deserialize<NetUserId>(mem);
 //     }
 
-//     // Ответ с Discord ID
-//     public sealed class MsgReceiveDiscordId : NetMessage
+//     public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer)
 //     {
-//         public NetUserId UserId { get; set; }
-//         public string? DiscordId { get; set; }
-
-//         public override MsgGroups MsgGroup => MsgGroups.Command;
-
-//         public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer)
-//         {
-//             UserId = buffer.ReadUserId();
-//             DiscordId = buffer.ReadString();
-//         }
-
-//         public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer)
-//         {
-//             buffer.Write(UserId);
-//             buffer.Write(DiscordId);
-//         }
+//         using var mem = new MemoryStream();
+//         serializer.Serialize(mem, UserId);
+//         buffer.Write(mem.ToArray());
 //     }
+
+//     public override NetDeliveryMethod DeliveryMethod => NetDeliveryMethod.ReliableOrdered;
+// }
+
+// /// <summary>
+// /// Сервер -> Клиент: Отправка Discord ID
+// /// </summary>
+// public sealed class MsgReceiveDiscordId : NetMessage
+// {
+//     public override MsgGroups MsgGroup => MsgGroups.Command;
+//     public NetUserId UserId;
+//     public string? DiscordId;
+
+//     public override void ReadFromBuffer(NetIncomingMessage buffer, IRobustSerializer serializer)
+//     {
+//         using var mem = new MemoryStream(buffer.ReadBytes(buffer.LengthBytes));
+//         UserId = serializer.Deserialize<NetUserId>(mem);
+//         DiscordId = serializer.Deserialize<string?>(mem);
+//     }
+
+//     public override void WriteToBuffer(NetOutgoingMessage buffer, IRobustSerializer serializer)
+//     {
+//         using var mem = new MemoryStream();
+//         serializer.Serialize(mem, UserId);
+//         serializer.Serialize(mem, DiscordId!);
+//         buffer.Write(mem.ToArray());
+//     }
+
+//     public override NetDeliveryMethod DeliveryMethod => NetDeliveryMethod.ReliableOrdered;
 // }
