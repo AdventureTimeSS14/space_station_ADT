94655c89a270a141400aba29d35a6d0b194790c2
CrimeMoot <wakeafa@gmail.com>
2025-06-14 01:18:54 +0500

    test (#182)

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -40,6 +40,8 @@ namespace Content.IntegrationTests.Tests
                     .Where(p => !pair.IsTestPrototype(p))
                     .Where(p => !p.Components.ContainsKey("MapGrid")) // This will smash stuff otherwise.
                     .Where(p => !p.Components.ContainsKey("RoomFill")) // This comp can delete all entities, and spawn others
+                    .Where(p => !p.ID.Contains("ADTAlwaysPoweredShuttle")) // Ganimed edit
+                    .Where(p => !p.ID.Contains("ImmovableRod")) // Ganimed edit
                     .Select(p => p.ID)
                     .ToList();
 
@@ -103,6 +105,8 @@ namespace Content.IntegrationTests.Tests
                     .Where(p => !pair.IsTestPrototype(p))
                     .Where(p => !p.Components.ContainsKey("MapGrid")) // This will smash stuff otherwise.
                     .Where(p => !p.Components.ContainsKey("RoomFill")) // This comp can delete all entities, and spawn others
+                    .Where(p => !p.ID.Contains("ADTAlwaysPoweredShuttle")) // Ganimed edit
+                    .Where(p => !p.ID.Contains("ImmovableRod")) // Ganimed edit
                     .Select(p => p.ID)
                     .ToList();
                 foreach (var protoId in protoIds)
