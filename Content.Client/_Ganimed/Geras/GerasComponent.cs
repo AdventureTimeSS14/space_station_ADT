01147fef92baf0c7ea5ca3274c5cbe311bc00c86
CrimeMoot <wakeafa@gmail.com>
2025-07-02 15:28:16 +0500

    Geras (#236)

---
+++ Content.Client/_Ganimed/Geras/GerasComponent.cs
@@ -0,0 +1,8 @@
+using Content.Shared._Ganimed.Geras;
+
+namespace Content.Client._Ganimed.Geras.Component;
+
+[RegisterComponent]
+public sealed partial class GerasComponent : SharedGerasComponent
+{
+}
