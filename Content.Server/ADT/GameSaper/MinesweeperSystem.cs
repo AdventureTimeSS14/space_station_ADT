d7e264b66ba06ec4dc6ec6d0a93bae56869e1e58
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-08-25 12:51:15 +0300

    - refactor(src): Рефактор MinesweeperSystem

--- Content.Server/ADT/GameSaper/MinesweeperSystem.cs
+++ Content.Server/ADT/GameSaper/MinesweeperSystem.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-using Content.Server.Popups;
 using Content.Shared.ADT.Minesweeper;
 
 namespace Content.Server.ADT.Minesweeper;
@@ -7,9 +5,7 @@ namespace Content.Server.ADT.Minesweeper;
 public sealed partial class MinesweeperSystem : EntitySystem
 {
     [Dependency] private readonly IEntityManager _entityManager = default!;
-    [Dependency] private readonly PopupSystem _popupSystem = default!;
 
-    private string _userName = "";
     public override void Initialize()
     {
         base.Initialize();
@@ -23,39 +19,26 @@ public sealed partial class MinesweeperSystem : EntitySystem
 
     private void OnBoundUiOpened(EntityUid uid, MinesweeperComponent component, BoundUIOpenedEvent args)
     {
-        // args.Session — это игрок, открывший интерфейс
         var actor = args.Actor;
-        var name = _entityManager.GetComponent<MetaDataComponent>(actor);
-
-        // if (component.LastOpenedBy != null)
-        // {
-        //     var message = Loc.GetString("machine-already-in-use", ("machine", uid));
-        //     _popupSystem.PopupEntity(message, uid, args.Entity);
-        //     Logger.Warning($"{name.EntityName} Попытался открыть используемый {ToPrettyString(uid)} ");
-        //     return;
-        // }
-
-        Logger.Warning($"Игрок {name} открыл сапёра на объекте {ToPrettyString(uid)} name {name.EntityName}");
-        _userName = name.EntityName;
-        component.LastOpenedBy = name.EntityName;
-        Dirty(uid, component);
+        if (_entityManager.TryGetComponent<MetaDataComponent>(actor, out var meta))
+        {
+            component.LastOpenedBy = meta.EntityName;
+            Dirty(uid, component);
+        }
     }
 
     private void OnBoundUiClosed(EntityUid uid, MinesweeperComponent component, BoundUIClosedEvent args)
     {
-        var actor = args.Actor;
-        var name = _entityManager.GetComponent<MetaDataComponent>(actor);
-        Logger.Warning($"Игрок {name} ЗАКРЫЛ сапёра на объекте {ToPrettyString(uid)} name {name.EntityName}");
         component.LastOpenedBy = null;
         Dirty(uid, component);
     }
 
     private void OnWinMessageReceived(EntityUid uid, MinesweeperComponent component, MinesweeperWinMessage msg)
     {
-        Logger.Warning($"ПРИШЛО OnWinMessageReceived для {msg.NameWin}");
-
         // Проходим по всем сущностям с компонентом MinesweeperComponent
-        foreach (var comp in _entityManager.EntityQuery<MinesweeperComponent>())
+        // И при выйгрыше на одном из автоматов, передаём рекорд во все компоненты таких сущностей
+        var query = _entityManager.EntityQueryEnumerator<MinesweeperComponent>();
+        while (query.MoveNext(out var ent, out var comp))
         {
             comp.Records.Add(new MinesweeperRecord
             {
@@ -64,7 +47,7 @@ public sealed partial class MinesweeperSystem : EntitySystem
                 EntityName = msg.NameWin
             });
 
-            _entityManager.Dirty(comp.Owner, comp);
+            _entityManager.Dirty(ent, comp);
         }
     }
 }
