e5078503c5c7324e23a0d49ddc8beca604b6ee39
LightSurvivor <pystinaleksej36@gmail.com>
2025-09-15 14:15:00 +0300

    Update IanHeroEvent.cs

--- Content.Server/ADT/StationEvents/Components/IanHeroEvent.cs
+++ Content.Server/ADT/StationEvents/Components/IanHeroEvent.cs
@@ -1,6 +1,3 @@
-using Content.Server.StationEvents.Events;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Content.Server.ADT.StationEvents.Events;
 
 namespace Content.Server.ADT.StationEvents.Components;
