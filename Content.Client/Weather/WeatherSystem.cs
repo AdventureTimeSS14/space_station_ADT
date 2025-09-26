a14798f4124fd8664a240ec2e97a21a51369c4e8
CrimeMoot <wakeafa@gmail.com>
2025-09-26 17:47:45 +0500

    Weather fix

--- Content.Client/Weather/WeatherSystem.cs
+++ Content.Client/Weather/WeatherSystem.cs
@@ -4,6 +4,7 @@ using Content.Shared.Weather;
 using Robust.Client.Audio;
 using Robust.Client.GameObjects;
 using Robust.Client.Player;
+using Robust.Client.State; // Ganimed edit
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.GameStates;
 using Robust.Shared.Map;
@@ -19,7 +20,7 @@ public sealed class WeatherSystem : SharedWeatherSystem
     [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly MapSystem _mapSystem = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
-
+    [Dependency] private readonly IStateManager _stateManager = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -32,8 +33,21 @@ public sealed class WeatherSystem : SharedWeatherSystem
 
         var ent = _playerManager.LocalEntity;
 
-        if (ent == null)
+        // Ganimed edit start
+        if (!IsInGameState())
+        {
+            weather.Stream = _audio.Stop(weather.Stream);
             return;
+        }
+        // Ganimed edit end
+
+        if (ent == null)
+        // Ganimed edit start
+        {
+            weather.Stream = _audio.Stop(weather.Stream);
+            return;
+        }
+        // Ganimed edit end
 
         var mapUid = Transform(uid).MapUid;
         var entXform = Transform(ent.Value);
@@ -130,11 +144,27 @@ public sealed class WeatherSystem : SharedWeatherSystem
         if (!Timing.IsFirstTimePredicted)
             return true;
 
+        // Ganimed edit start
+        if (!IsInGameState())
+        {
+            weather.Stream = _audio.Stop(weather.Stream);
+            return true;
+        }
+        // Ganimed edit end
+
         // TODO: Fades (properly)
         weather.Stream = _audio.Stop(weather.Stream);
         weather.Stream = _audio.PlayGlobal(weatherProto.Sound, Filter.Local(), true)?.Entity;
         return true;
     }
+    // Ganimed edit start
+    private bool IsInGameState()
+    {
+        var currentState = _stateManager.CurrentState;
+        return currentState?.GetType().Name != "LobbyState" &&
+               _playerManager.LocalEntity != null;
+    }
+    // Ganimed edit end
 
     private void OnWeatherHandleState(EntityUid uid, WeatherComponent component, ref ComponentHandleState args)
     {
