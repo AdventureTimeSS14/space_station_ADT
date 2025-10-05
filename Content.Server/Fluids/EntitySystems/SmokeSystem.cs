4480584db154e4638b09bca7dcbfb23947b7f876
RevengenRat <138193222+Ratyyy@users.noreply.github.com>
2025-10-05 16:29:48 +0300

    пробуем чинить дым

--- Content.Server/Fluids/EntitySystems/SmokeSystem.cs
+++ Content.Server/Fluids/EntitySystems/SmokeSystem.cs
@@ -82,12 +82,12 @@ public sealed class SmokeSystem : EntitySystem
 
     private void OnStartCollide(Entity<SmokeComponent> entity, ref StartCollideEvent args)
     {
-        if (_smokeAffectedQuery.HasComponent(args.OtherEntity))
-            return;
-        // ADT Tester fix start
+        // #ADT-tweak: Don't add components to terminating entities
         if (Deleted(args.OtherEntity) || Terminating(args.OtherEntity))
             return;
-        // ADT Tester fix end
+
+        if (_smokeAffectedQuery.HasComponent(args.OtherEntity))
+            return;
 
         var smokeAffected = AddComp<SmokeAffectedComponent>(args.OtherEntity);
         smokeAffected.SmokeEntity = entity;
@@ -96,6 +96,10 @@ public sealed class SmokeSystem : EntitySystem
 
     private void OnEndCollide(Entity<SmokeComponent> entity, ref EndCollideEvent args)
     {
+        // #ADT-tweak: Don't process terminating entities
+        if (Deleted(args.OtherEntity) || Terminating(args.OtherEntity))
+            return;
+
         // if we are already in smoke, make sure the thing we are exiting is the current smoke we are in.
         if (_smokeAffectedQuery.TryGetComponent(args.OtherEntity, out var smokeAffectedComponent))
         {
@@ -116,10 +120,9 @@ public sealed class SmokeSystem : EntitySystem
             if (!_smokeQuery.HasComponent(ent))
                 continue;
 
-            // ADT Tester fix start
+            // #ADT-tweak: Don't reattach to terminating smoke
             if (Deleted(ent) || Terminating(ent))
                 continue;
-            // ADT Tester fix end
 
             smokeAffectedComponent ??= EnsureComp<SmokeAffectedComponent>(args.OtherEntity);
             smokeAffectedComponent.SmokeEntity = ent;
