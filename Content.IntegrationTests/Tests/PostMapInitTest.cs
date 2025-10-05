f470a9da44bbec8a1b4009798957aa3f88d4702b
RevengenRat <138193222+Ratyyy@users.noreply.github.com>
2025-10-05 21:57:20 +0300

    нет центкома

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -112,8 +112,7 @@ namespace Content.IntegrationTests.Tests
             "ADT_Aspid",
             "ADT_Cluster_Legacy",
             "ADT_Meta",
-            "ADT_Origin",
-            "ADT_Centcomm"
+            "ADT_Origin"
             // ADT-End
         };
 
