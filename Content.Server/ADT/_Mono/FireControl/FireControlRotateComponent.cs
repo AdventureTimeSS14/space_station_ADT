846cf333c1a5c332f7d703b280f4750580a40d6c
Red-Lis <alexander.kratchuk@gmail.com>
2025-11-08 22:32:49 +0700

    Пушки?

---
+++ Content.Server/ADT/_Mono/FireControl/FireControlRotateComponent.cs
@@ -0,0 +1,7 @@
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Server.ADT._Mono.FireControl;
+
+[RegisterComponent]
+public sealed partial class FireControlRotateComponent : Component;
+// Give this to ship guns you want to rotate when firing.
