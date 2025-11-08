ab87f1b2d65871395d5a02f236ba4198a8f6b30e
Red-Lis <alexander.kratchuk@gmail.com>
2025-11-09 02:03:31 +0700

    Просто нахуй, а щиты вроде работают так что

--- Content.Server/ADT/_Mono/FireControl/FireControlGridComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server.ADT._Mono.FireControl;
-
-[RegisterComponent]
-public sealed partial class FireControlGridComponent : Component
-{
-    [ViewVariables]
-    public EntityUid? ControllingServer = null;
-}
--- Content.Shared/ADT/_Mono/SpaceArtillery/ShipWeaponProjectileComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Shared.ADT._Mono.SpaceArtillery;
-
-[RegisterComponent]
-public sealed partial class ShipWeaponProjectileComponent : Component
-{
-
-}
