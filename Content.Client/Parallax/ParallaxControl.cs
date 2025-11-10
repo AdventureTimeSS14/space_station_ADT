dcf6eec379126c3c4ddac7477c0722ef7bb3c91b
Unlumy <mr.rain01@yandex.ru>
2025-11-10 09:28:42 +0300

    Комментарий

--- Content.Client/Parallax/ParallaxControl.cs
+++ Content.Client/Parallax/ParallaxControl.cs
@@ -19,7 +19,7 @@ public sealed class ParallaxControl : Control
     [Dependency] private readonly IParallaxManager _parallaxManager = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
 
-    private string _parallaxPrototype = "NesGreen";
+    private string _parallaxPrototype = "NesGreen"; // ADT-Tweak: Изменён стандартный параллакс на зелёный с планетой
 
     [ViewVariables(VVAccess.ReadWrite)] public Vector2 Offset { get; set; }
     [ViewVariables(VVAccess.ReadWrite)] public float SpeedX { get; set; } = 0.0f;
