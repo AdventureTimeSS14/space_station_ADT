fedcdeae277dd3761d6888cb525f4308c62ee9d5
LightSurvivor <pystinaleksej36@gmail.com>
2025-11-16 08:25:02 +0300

    bso

---
+++ Content.Server/ADT/BlueShield/BlueShieldShootingSystem.cs
@@ -0,0 +1,30 @@
+using Content.Server.AlertLevel;
+using Content.Shared.ADT.CantShoot;
+using Content.Shared.Weapons.Reflect;
+
+namespace Content.Server.ADT.BlueShield;
+
+public sealed class BlueShieldShootingSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<AlertLevelChangedEvent>(BSOCanShoot);
+    }
+    private void BSOCanShoot(AlertLevelChangedEvent args)
+    {
+        if (args.AlertLevel == "gamma" || args.AlertLevel == "epsilon" || args.AlertLevel == "delta")
+        {
+            // Находим все сущности с CantShootComponent
+            var query = EntityQueryEnumerator<CantShootComponent>();
+            while (query.MoveNext(out var uid, out _))
+            {
+                // Делаем так, чтобы спящие карпы не подвергались этой системе
+                if (TryComp<ReflectComponent>(uid, out var comp) && comp.ReflectProb == 1)
+                    return;
+                // Удаляем с сущности CantShootComponent
+                RemComp<CantShootComponent>(uid);
+            }
+        }
+    }
+}
