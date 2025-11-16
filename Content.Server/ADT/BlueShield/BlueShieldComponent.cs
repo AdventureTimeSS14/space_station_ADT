14c92c97ec11bd354590d0738b3e10b0bd64893c
LightSurvivor <pystinaleksej36@gmail.com>
2025-11-16 18:21:57 +0300

    Update BlueShieldComponent.cs

--- Content.Server/ADT/BlueShield/BlueShieldComponent.cs
+++ Content.Server/ADT/BlueShield/BlueShieldComponent.cs
@@ -1,11 +1,3 @@
-using Content.Shared.Whitelist;
-using Robust.Shared.Audio;
-using Robust.Shared.Containers;
-using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
-
 namespace Content.Server.ADT.BlueShield.Components;
 
 [RegisterComponent]
