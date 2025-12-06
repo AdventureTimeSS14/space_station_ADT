9a3670437a45cc86c189f24fe8953112078a588f
FaDeOkno <logkedr18@gmail.com>
2025-12-06 04:20:57 +0400

    refactor(src, ui): БАшой реворк менюшки

---
+++ Content.Client/ADT/CharecterFlavor/CharacterFlavorUiController.cs
@@ -0,0 +1,39 @@
+using Robust.Client.UserInterface;
+using JetBrains.Annotations;
+using Robust.Client.UserInterface.Controllers;
+using Robust.Shared.GameStates;
+using Content.Client.Gameplay;
+
+namespace Content.Client.ADT.CharecterFlavor;
+
+[UsedImplicitly]
+public sealed class CharacterFlavorUiController : UIController, IOnStateEntered<GameplayState>
+{
+    [ViewVariables]
+    private CharacterFlavorWindow? _window;
+
+    private NetEntity _currentEntity;
+
+    public void OnStateEntered(GameplayState state)
+    {
+        _window?.Close();
+        _window = null;
+    }
+
+    public void OpenMenu(EntityUid target)
+    {
+        _window = UIManager.CreateWindow<CharacterFlavorWindow>();
+        _window.SetEntity(target);
+        _window.OpenCentered();
+
+        _currentEntity = EntityManager.GetNetEntity(target);
+    }
+
+    public void SetHeadshot(NetEntity target, byte[] image)
+    {
+        if (target != _currentEntity)
+            return;
+
+        _window?.SetHeadshot(image);
+    }
+}
