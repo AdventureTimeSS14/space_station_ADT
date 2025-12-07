d12c652a3489f02d8a55b3cdac1a92c746d98a12
RevengenRat <138193222+Ratyyy@users.noreply.github.com>
2025-12-07 13:37:58 +0200

    аыаауыв

--- Content.Server/ADT/CharecterFlavor/CharecterFlavorSystem.cs
+++ Content.Server/ADT/CharecterFlavor/CharecterFlavorSystem.cs
@@ -1,5 +1,6 @@
 // Inspired by Nyanotrasen
 using Content.Shared.ADT.CharecterFlavor;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -7,15 +8,41 @@ namespace Content.Server.ADT.CharecterFlavor;
 
 public sealed class CharecterFlavorSystem : SharedCharecterFlavorSystem
 {
-    private static readonly HttpClient HttpClient = new HttpClient();
+    private static HttpClient? _httpClient;
+
+    private const string ProxyUrl = "http://proxy.example.com:8080";
+    private const string ProxyUsername = "";
+    private const string ProxyPassword = "";
 
     public override void Initialize()
     {
         base.Initialize();
 
-        if (HttpClient.DefaultRequestHeaders.UserAgent.Count == 0)
+        if (_httpClient == null)
         {
-            HttpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0");
+            var handler = new HttpClientHandler();
+
+            // Настройка прокси если URL указан
+            if (!string.IsNullOrEmpty(ProxyUrl))
+            {
+                if (string.IsNullOrEmpty(ProxyUsername))
+                {
+                    handler.Proxy = new WebProxy(ProxyUrl);
+                }
+                else
+                {
+                    handler.Proxy = new WebProxy
+                    {
+                        Address = new Uri(ProxyUrl),
+                        Credentials = new NetworkCredential(ProxyUsername, ProxyPassword)
+                    };
+                }
+                handler.ServerCertificateCustomValidationCallback = 
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            }
+
+            _httpClient = new HttpClient(handler);
+            _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0");
         }
     }
 
@@ -42,7 +69,7 @@ public sealed class CharecterFlavorSystem : SharedCharecterFlavorSystem
     {
         try
         {
-            return await HttpClient.GetByteArrayAsync(url);
+            return await _httpClient!.GetByteArrayAsync(url);
         }
         catch (Exception ex)
         {
