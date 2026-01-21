f01f5900b6e5db647e528c9ab8f289061e0b9b5c
Firemixx <firemixorig@gmail.com>
2026-01-21 09:42:21 +0200

    fix

--- Content.Shared/ADT/StampHit/SharedStampHitSystem.cs
+++ Content.Shared/ADT/StampHit/SharedStampHitSystem.cs
@@ -15,13 +15,12 @@ public abstract partial class SharedStampHitSystem : EntitySystem
     private void Examined(EntityUid examinedUid, StampedEntityComponent stampedComp, ExaminedEvent args)
     {
         var locUser = ("user", Identity.Entity(examinedUid, EntityManager));
-+        List<string> readyListStamped = [];
-+        foreach (var stamp in stampedComp.StampToEntity)
-+        {
-+            readyListStamped.Add(Loc.GetString(stamp));
-         }
-+        var locStamps = ("stamps", ContentLocalizationManager.FormatList(readyListStamped));
-+        args.PushMarkup(Loc.GetString("comp-stamp-examine", locUser, locStamps));
+        List<string> readyListStamped = [];
+        foreach (var stamp in stampedComp.StampToEntity)
+        {
+            readyListStamped.Add(Loc.GetString(stamp));
         }
+        var locStamps = ("stamps", ContentLocalizationManager.FormatList(readyListStamped));
+        args.PushMarkup(Loc.GetString("comp-stamp-examine", locUser, locStamps));
     }
 }
