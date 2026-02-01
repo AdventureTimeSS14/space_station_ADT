103643688d62d87366cc04c72add08c988dbeab3
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2026-02-01 12:35:43 +0300

    fix: Fixed SandevistanImplantSystem

--- Content.Server/ADT/Sandevistan/SandevistanImplantSystem.cs
+++ Content.Server/ADT/Sandevistan/SandevistanImplantSystem.cs
@@ -20,8 +20,7 @@ public sealed class SandevistanImplantSystem : EntitySystem
 
     private void OnImplanted(EntityUid uid, SandevistanImplantComponent comp, ref ImplantImplantedEvent args)
     {
-        if (args.Implanted is not { } owner)
-            return;
+        var owner = args.Implanted;
 
         EnsureComp<SandevistanUserComponent>(owner);
         EnsureComp<GrabProtectionComponent>(owner);
@@ -34,8 +33,7 @@ public sealed class SandevistanImplantSystem : EntitySystem
 
     private void OnRemoved(EntityUid uid, SandevistanImplantComponent comp, ref ImplantRemovedEvent args)
     {
-        if (args.Implanted is not { } owner)
-            return;
+        var owner = args.Implanted;
 
         if (TryComp<SandevistanUserComponent>(owner, out var user))
         {
