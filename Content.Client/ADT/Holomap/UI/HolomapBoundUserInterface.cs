ba389fbeb79feb77644a5ba071e6d908f4f0cfe8
IIo3u <bylov4@yandex.ru>
2026-02-03 04:10:51 +0300

    add(.yml-rsi-ftl, .cs, .xaml) - Голокарта

---
+++ Content.Client/ADT/Holomap/UI/HolomapBoundUserInterface.cs
@@ -0,0 +1,49 @@
+using Content.Shared.ADT.Holomap;
+using JetBrains.Annotations;
+using Robust.Client.UserInterface;
+
+namespace Content.Client.ADT.Holomap.UI;
+
+[UsedImplicitly]
+public sealed class HolomapBoundUserInterface : BoundUserInterface
+{
+    [ViewVariables]
+    private HolomapWindow? _window;
+
+    public HolomapBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = new HolomapWindow();
+        _window.OnClose += Close;
+        _window.OnModeSelected += OnModeSelected;
+        _window.OpenCentered();
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+        if (state is HolomapBoundUserInterfaceState uiState)
+        {
+            _window?.UpdateState(uiState);
+        }
+    }
+
+    private void OnModeSelected(HolomapMode mode)
+    {
+        SendMessage(new HolomapModeSelectedMessage(mode));
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (disposing)
+        {
+            _window?.Dispose();
+        }
+    }
+}
