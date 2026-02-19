1d9c69a1832873b22a0a57eb47af70177a7d10a7
Darkiich <ne>
2026-02-19 17:42:37 +0300

    Закомментировал компонент, который нигде не используется и валит тесты

--- Content.Shared/ADT/Emp/Components/EmpContainerProtectionComponent.cs
+++ Content.Shared/ADT/Emp/Components/EmpContainerProtectionComponent.cs
@@ -1,13 +1,13 @@
-using Content.Shared.ADT.EMP;
-using Robust.Shared.GameStates;
+// using Content.Shared.ADT.EMP;
+// using Robust.Shared.GameStates;
 
-namespace Content.Shared.ADT.EMP;
+// namespace Content.Shared.ADT.EMP;
 
-[RegisterComponent, NetworkedComponent]
-public sealed partial class EmpContainerProtectionComponent : Component
-{
-    public List<EntityUid> Batteries = new();
+// [RegisterComponent, NetworkedComponent]
+// public sealed partial class EmpContainerProtectionComponent : Component
+// {
+//     public List<EntityUid> Batteries = new();
 
-    [DataField]
-    public string ContainerId = "cell_slot";
-}
+//     [DataField]
+//     public string ContainerId = "cell_slot";
+// }
--- Content.Shared/ADT/Emp/Systems/EmpDisablingSystem.cs
+++ Content.Shared/ADT/Emp/Systems/EmpDisablingSystem.cs
@@ -13,10 +13,10 @@ public abstract partial class SharedEmpSystem
         SubscribeLocalEvent<EmpDisablingComponent, EmpPulseEvent>(OnDisablingPulse);
         SubscribeLocalEvent<EmpProtectionComponent, EmpAttemptEvent>(OnProtectionEmp);
 
-        SubscribeLocalEvent<EmpContainerProtectionComponent, EntInsertedIntoContainerMessage>(OnInserted);
-        SubscribeLocalEvent<EmpContainerProtectionComponent, EntRemovedFromContainerMessage>(OnEjected);
-        SubscribeLocalEvent<EmpContainerProtectionComponent, ComponentShutdown>(OnShutdown);
-        SubscribeLocalEvent<EmpContainerProtectionComponent, MapInitEvent>(OnInit);
+        // SubscribeLocalEvent<EmpContainerProtectionComponent, EntInsertedIntoContainerMessage>(OnInserted);
+        // SubscribeLocalEvent<EmpContainerProtectionComponent, EntRemovedFromContainerMessage>(OnEjected);
+        // SubscribeLocalEvent<EmpContainerProtectionComponent, ComponentShutdown>(OnShutdown);
+        // SubscribeLocalEvent<EmpContainerProtectionComponent, MapInitEvent>(OnInit);
     }
 
     private void OnDisablingPulse(EntityUid uid, EmpDisablingComponent component, ref EmpPulseEvent args)
@@ -30,43 +30,43 @@ public abstract partial class SharedEmpSystem
         args.Cancelled = true;
     }
 
-    private void OnInserted(EntityUid uid, EmpContainerProtectionComponent component, ref EntInsertedIntoContainerMessage args)
-    {
-        if (HasComp<EmpProtectionComponent>(args.Entity))
-            return;
+    // private void OnInserted(EntityUid uid, EmpContainerProtectionComponent component, ref EntInsertedIntoContainerMessage args)
+    // {
+    //     if (HasComp<EmpProtectionComponent>(args.Entity))
+    //         return;
 
-        EnsureComp<EmpProtectionComponent>(args.Entity);
-        component.Batteries.Add(args.Entity);
-    }
+    //     EnsureComp<EmpProtectionComponent>(args.Entity);
+    //     component.Batteries.Add(args.Entity);
+    // }
 
-    private void OnEjected(EntityUid uid, EmpContainerProtectionComponent component, ref EntRemovedFromContainerMessage args)
-    {
-        if (!component.Batteries.Contains(args.Entity))
-            return;
+    // private void OnEjected(EntityUid uid, EmpContainerProtectionComponent component, ref EntRemovedFromContainerMessage args)
+    // {
+    //     if (!component.Batteries.Contains(args.Entity))
+    //         return;
 
-        RemComp<EmpProtectionComponent>(args.Entity);
-        component.Batteries.Remove(args.Entity);
-    }
+    //     RemComp<EmpProtectionComponent>(args.Entity);
+    //     component.Batteries.Remove(args.Entity);
+    // }
 
-    private void OnShutdown(EntityUid uid, EmpContainerProtectionComponent component, ComponentShutdown args)
-    {
-        foreach (var item in component.Batteries)
-            RemComp<EmpProtectionComponent>(item);
-    }
+    // private void OnShutdown(EntityUid uid, EmpContainerProtectionComponent component, ComponentShutdown args)
+    // {
+    //     foreach (var item in component.Batteries)
+    //         RemComp<EmpProtectionComponent>(item);
+    // }
 
-    private void OnInit(EntityUid uid, EmpContainerProtectionComponent component, MapInitEvent args)
-    {
-        var containers = _container.GetAllContainers(uid);
-        foreach (var container in containers)
-        {
-            foreach (var item in container.ContainedEntities)
-            {
-                if (!HasComp<BatteryComponent>(item) || HasComp<EmpProtectionComponent>(item))
-                    continue;
+    // private void OnInit(EntityUid uid, EmpContainerProtectionComponent component, MapInitEvent args)
+    // {
+    //     var containers = _container.GetAllContainers(uid);
+    //     foreach (var container in containers)
+    //     {
+    //         foreach (var item in container.ContainedEntities)
+    //         {
+    //             if (!HasComp<BatteryComponent>(item) || HasComp<EmpProtectionComponent>(item))
+    //                 continue;
 
-                EnsureComp<EmpProtectionComponent>(item);
-                component.Batteries.Add(item);
-            }
-        }
-    }
+    //             EnsureComp<EmpProtectionComponent>(item);
+    //             component.Batteries.Add(item);
+    //         }
+    //     }
+    // }
 }
