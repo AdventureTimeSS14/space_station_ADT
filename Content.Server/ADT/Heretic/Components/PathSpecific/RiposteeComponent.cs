dc27a1f6961d1863b9ba7f8a510edf368ea0f01e
CrimeMoot <wakeafa@gmail.com>
2026-03-01 21:38:00 +0500

    tweak

--- Content.Server/ADT/Heretic/Components/PathSpecific/RiposteeComponent.cs
+++ Content.Server/ADT/Heretic/Components/PathSpecific/RiposteeComponent.cs
@@ -3,7 +3,7 @@ namespace Content.Server.Heretic.Components.PathSpecific;
 [RegisterComponent]
 public sealed partial class RiposteeComponent : Component
 {
-[DataField] public float Cooldown = 5f;
+    [DataField] public float Cooldown = 5f;
     [ViewVariables(VVAccess.ReadWrite)] public float Timer = 5f;
 
     [DataField] public bool CanRiposte = true;
