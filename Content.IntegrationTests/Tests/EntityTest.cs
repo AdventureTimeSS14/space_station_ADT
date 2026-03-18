640d932ca14e15549d53ca355614ee41e6f00ec8
CrimeMoot <wakeafa@gmail.com>
2026-03-18 23:40:47 +0500

    fix TechAnomaly

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -44,6 +44,7 @@ namespace Content.IntegrationTests.Tests
                 .Where(p => !p.Components.ContainsKey("MobReplacementRule")) // ADT-tweak - fuck them mimics
                 .Where(p => !p.Components.ContainsKey("Supermatter")) // ADT-tweak - Supermatter eats everything, oh no!
                 .Where(p => !p.Components.ContainsKey("RoomFill")) // This comp can delete all entities, and spawn others
+                .Where(p => !p.Components.ContainsKey("TechAnomaly")) // ADT-tweak - TechAnomaly can trigger guns via DeviceLink, causing spawn on invalid coords
                 .Select(p => p.ID)
                 .ToList();
             // ADT-tweak end
@@ -146,6 +147,7 @@ namespace Content.IntegrationTests.Tests
                     .Where(p => !p.Components.ContainsKey("MapGrid")) // This will smash stuff otherwise.
                     .Where(p => !p.Components.ContainsKey("Supermatter")) // ADT-tweak - Supermatter eats everything, oh no!
                     .Where(p => !p.Components.ContainsKey("RoomFill")) // This comp can delete all entities, and spawn others
+                    .Where(p => !p.Components.ContainsKey("TechAnomaly")) // ADT-tweak - TechAnomaly can trigger guns via DeviceLink, causing spawn on invalid coords
                     .Select(p => p.ID)
                     .ToList();
                 foreach (var protoId in protoIds)
@@ -456,6 +458,7 @@ namespace Content.IntegrationTests.Tests
                 "DebugExceptionStartup",
                 "GridFill",
                 "RoomFill",
+                "TechAnomaly", // ADT-tweak - TechAnomaly can trigger guns via DeviceLink, causing spawn on invalid coords
                 "Map", // We aren't testing a map entity in this test
                 "MapGrid",
                 "Broadphase",
