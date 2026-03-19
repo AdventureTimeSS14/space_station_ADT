0f7319aedc4a72d6afc60242be0c643d112a9590
kerfus-owo <g123412345125345@mail.ru>
2026-03-19 13:26:23 +0500

    Посоветовал хуйни

--- Content.Server/Medical/CrewMonitoring/CrewMonitoringServerSystem.cs
+++ Content.Server/Medical/CrewMonitoring/CrewMonitoringServerSystem.cs
@@ -83,7 +83,7 @@ public sealed class CrewMonitoringServerSystem : EntitySystem
         foreach (var (address, sensor) in component.SensorStatus)
         {
             var dif = _gameTiming.CurTime - sensor.Timestamp;
-            if (dif.TotalSeconds > component.SensorTimeout)
+            if (dif.Seconds > component.SensorTimeout)
                 toRemove.Add(address);
         }
 
