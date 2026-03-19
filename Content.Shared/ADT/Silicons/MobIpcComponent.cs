107f7be4994d0c5cab16103ae0749af43410367f
kerfus-owo <g123412345125345@mail.ru>
2026-03-19 08:32:18 +0500

    сойдёт

--- Content.Shared/ADT/Silicons/MobIpcComponent.cs
+++ Content.Shared/ADT/Silicons/MobIpcComponent.cs
@@ -1,7 +1,14 @@
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+
 namespace Content.Shared.ADT.Silicon;
 
 [RegisterComponent]
 public sealed partial class MobIpcComponent : Component
 {
+    [DataField]
+    public bool DisablePointLightOnDeath = false;
 
+    [DataField]
+    public bool LightDisabledByDeath;
 }
