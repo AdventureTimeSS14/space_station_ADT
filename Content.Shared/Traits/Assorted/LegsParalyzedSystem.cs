9c6bce16f0f853d612a23c9d391e2208c363e76b
CrimeMoot <wakeafa@gmail.com>
2026-04-04 23:24:39 +0500

    add fix

--- Content.Shared/Traits/Assorted/LegsParalyzedSystem.cs
+++ Content.Shared/Traits/Assorted/LegsParalyzedSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Content.Shared.Body.Systems;
 using Content.Shared.Buckle.Components;
+using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Events;
 using Content.Shared.Movement.Systems;
 using Content.Shared.Standing;
@@ -50,6 +51,9 @@ public sealed class LegsParalyzedSystem : EntitySystem
     {
         // ADT-Tweak start
         if (HasComp<RiderComponent>(uid))
+            return;
+
+        if (HasComp<RelayInputMoverComponent>(uid))
             return;
         // ADT-Tweak end
 
