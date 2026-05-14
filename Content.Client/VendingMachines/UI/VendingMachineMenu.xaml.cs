79e25143fdc82eb12283b33b2f3ac4f16f48ff3d
CrimeMoot <Wakeafa@gmail.com>
2026-05-14 22:18:06 +0500

    fix vending

--- Content.Client/VendingMachines/UI/VendingMachineMenu.xaml.cs
+++ Content.Client/VendingMachines/UI/VendingMachineMenu.xaml.cs
@@ -17,10 +17,15 @@ namespace Content.Client.VendingMachines.UI
     [GenerateTypedNameReferences]
     public sealed partial class VendingMachineMenu : FancyWindow
     {
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-        [Dependency] private readonly IEntityManager _entityManager = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly IEntityManager _entityManager = default!;
 
-        private readonly Dictionary<EntProtoId, uint> _amounts = new();
+    /// <summary>
+    /// Whether the vending machine is able to be interacted with or not.
+    /// </summary>
+    private bool _enabled;
+
+    private readonly Dictionary<EntProtoId, uint> _amounts = new();
 
         private readonly Dictionary<EntProtoId, EntityUid> _dummies = [];
         private EntityUid _entityUid;
@@ -111,7 +116,7 @@ namespace Content.Client.VendingMachines.UI
 
             button.AddChild(item);
             button.AddStyleClass(StyleClass.ButtonSquare);
-            button.Disabled = _amounts[protoID] == 0;
+            button.Disabled = !_enabled || _amounts[protoID] == 0;
 
             button.ToolTip = text;
         }
