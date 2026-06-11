4d7c890868aa17a31a89aca330b8307e096c6c5e
Minokaa <247912484+MinokaaUWU@users.noreply.github.com>
2026-06-11 10:21:26 +0700

    fix GrabReleaseBindSystem

--- Content.Shared/ADT/GrabReleaseBind/GrabReleaseBindSystem.cs
+++ Content.Shared/ADT/GrabReleaseBind/GrabReleaseBindSystem.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 August Eymann <august.eymann@gmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.ActionBlocker;
 using Content.Shared.Input;
 using Content.Shared.Movement.Pulling.Components;
@@ -7,11 +12,15 @@ using Robust.Shared.Player;
 
 namespace Content.Shared.ADT.GrabReleaseBind;
 
+/// <summary>
+/// This handle binding the resist grab key
+/// </summary>
 public sealed class GrabReleaseBindSystem : EntitySystem
 {
     [Dependency] private readonly PullingSystem _pullingSystem = default!;
     [Dependency] private readonly ActionBlockerSystem _blocker = default!;
 
+    /// <inheritdoc/>
     public override void Initialize()
     {
         CommandBinds.Builder
