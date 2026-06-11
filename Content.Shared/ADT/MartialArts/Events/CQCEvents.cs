f4d58ef5737ce99b6b7487badeb514148657113f
Minokaa <247912484+MinokaaUWU@users.noreply.github.com>
2026-06-11 10:21:56 +0700

    fix CQCEvents

--- Content.Shared/ADT/MartialArts/Events/CQCEvents.cs
+++ Content.Shared/ADT/MartialArts/Events/CQCEvents.cs
@@ -1,3 +1,11 @@
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Aidenkrz <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2025 Lincoln McQueen <lincoln.mcqueen@gmail.com>
+// SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.ADT.MartialArts;
@@ -6,7 +14,7 @@ namespace Content.Shared.ADT.MartialArts;
 public sealed partial class CqcSlamPerformedEvent : EntityEventArgs;
 
 [Serializable, NetSerializable, DataDefinition]
-public sealed partial class CqcKickPerformedEvent : EntityEventArgs;
+public sealed partial class  CqcKickPerformedEvent : EntityEventArgs;
 
 [Serializable, NetSerializable, DataDefinition]
 public sealed partial class CqcRestrainPerformedEvent : EntityEventArgs;
@@ -16,5 +24,3 @@ public sealed partial class CqcPressurePerformedEvent : EntityEventArgs;
 
 [Serializable, NetSerializable, DataDefinition]
 public sealed partial class CqcConsecutivePerformedEvent : EntityEventArgs;
-
-public sealed class CanDoCQCEvent : HandledEntityEventArgs;
