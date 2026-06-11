cbace375ae1b31c5108dd8190e1184a04e03eb36
Minokaa <247912484+MinokaaUWU@users.noreply.github.com>
2026-06-11 10:21:58 +0700

    fix NinjutsuEvents

--- Content.Shared/ADT/MartialArts/Events/NinjutsuEvents.cs
+++ Content.Shared/ADT/MartialArts/Events/NinjutsuEvents.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Aviu00 <aviu00@protonmail.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Audio;
 using Robust.Shared.Serialization;
 
@@ -8,6 +13,12 @@ public abstract partial class BaseNinjutsuEvent : EntityEventArgs
 {
     [DataField]
     public virtual SoundSpecifier Sound { get; set; } = new SoundPathSpecifier("/Audio/Weapons/genhit3.ogg");
+}
+
+public sealed partial class NinjutsuTakedownPerformedEvent : BaseNinjutsuEvent
+{
+    [DataField]
+    public float BackstabMultiplier = 2.5f;
 }
 
 public sealed partial class BiteTheDustPerformedEvent : BaseNinjutsuEvent;
