af67f919aab391c7a8e68d7eac4bd3225d66d252
Minokaa <247912484+MinokaaUWU@users.noreply.github.com>
2026-06-11 10:21:57 +0700

    fix JudoEvents

--- Content.Shared/ADT/MartialArts/Events/JudoEvents.cs
+++ Content.Shared/ADT/MartialArts/Events/JudoEvents.cs
@@ -1,3 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Aidenkrz <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Lincoln McQueen <lincoln.mcqueen@gmail.com>
+// SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
+// SPDX-FileCopyrightText: 2025 pheenty <fedorlukin2006@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.ADT.MartialArts;
@@ -10,6 +22,12 @@ public sealed partial class JudoDiscombobulatePerformedEvent : EntityEventArgs
 
     [DataField]
     public float SpeedMultiplier = 0.7f;
+
+    [DataField]
+    public float StaminaResistanceModifier = 1.2f;
+
+    [DataField]
+    public EntProtoId StatusEffectProto = "StatusEffectStaminaResistanceModifier";
 }
 
 [Serializable, NetSerializable, DataDefinition]
