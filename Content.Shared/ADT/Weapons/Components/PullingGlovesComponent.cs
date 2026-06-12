0dfdf66d8edd111cfecf7d64d6d16098060ec0f5
iboy24367-eng <Iboy24367@gmail.com>
2026-06-12 21:49:14 +0300

    katana

---
+++ Content.Shared/ADT/Weapons/Components/PullingGlovesComponent.cs
@@ -0,0 +1,14 @@
+using Content.Shared.Actions;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.ADT.Weapon.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class PullingGlovesComponent : Component
+{
+    [DataField]
+    public EntProtoId Action = "ActionWeaponPull";
+
+    [DataField]
+    public EntityUid? ActionEntity;
+}
---
+++ Content.Shared/ADT/Weapons/Components/WeaponPullActionEvent.cs
@@ -0,0 +1,7 @@
+using Content.Shared.Actions;
+
+namespace Content.Shared.ADT.Weapon.Components;
+
+public sealed partial class WeaponPullActionEvent : InstantActionEvent
+{
+}
