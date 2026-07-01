eb93a7193fa6516d95d3911066095a41945e6df7
Golub4ik <97300528+Golub4ik-Official@users.noreply.github.com>
2026-07-01 16:33:36 +0300

    [FIX] Update RobustToolbox submodule to v277.2.1, add brain icon to uplink & fix icon visibility for different Master/Slave pairs

--- Content.Client/ADT/MindSlave/MindSlaveSystem.cs
+++ Content.Client/ADT/MindSlave/MindSlaveSystem.cs
@@ -1,11 +1,15 @@
 using Content.Shared.ADT.MindSlave.Components;
+using Content.Shared.Antag;
+using Content.Shared.StatusIcon;
 using Content.Shared.StatusIcon.Components;
+using Robust.Client.Player;
 using Robust.Shared.Prototypes;
 
 namespace Content.Client.ADT.MindSlave;
 
 public sealed class MindSlaveSystem : EntitySystem
 {
+    [Dependency] private readonly IPlayerManager _player = default!;
     [Dependency] private readonly IPrototypeManager _prototype = default!;
 
     public override void Initialize()
@@ -18,13 +22,55 @@ public sealed class MindSlaveSystem : EntitySystem
 
     private void GetSlaveIcon(Entity<MindSlaveComponent> ent, ref GetStatusIconsEvent args)
     {
-        if (_prototype.TryIndex(ent.Comp.StatusIcon, out var iconPrototype))
-            args.StatusIcons.Add(iconPrototype);
+        var viewer = _player.LocalEntity;
+        if (viewer == null)
+            return;
+
+        if (HasComp<ShowAntagIconsComponent>(viewer))
+        {
+            AddIcon(args, ent.Comp.StatusIcon);
+            return;
+        }
+
+        if (ent.Comp.Master == viewer)
+        {
+            AddIcon(args, ent.Comp.StatusIcon);
+            return;
+        }
+
+        if (TryComp<MindSlaveComponent>(viewer, out var viewerSlave) && viewerSlave.Master == ent.Comp.Master)
+        {
+            AddIcon(args, ent.Comp.StatusIcon);
+        }
     }
 
     private void GetMasterIcon(Entity<MindSlaveMasterComponent> ent, ref GetStatusIconsEvent args)
     {
-        if (_prototype.TryIndex(ent.Comp.StatusIcon, out var iconPrototype))
+        var viewer = _player.LocalEntity;
+        if (viewer == null)
+            return;
+
+        if (HasComp<ShowAntagIconsComponent>(viewer))
+        {
+            AddIcon(args, ent.Comp.StatusIcon);
+            return;
+        }
+
+        if (TryComp<MindSlaveComponent>(viewer, out var viewerSlave) && viewerSlave.Master == ent.Owner)
+        {
+            AddIcon(args, ent.Comp.StatusIcon);
+            return;
+        }
+
+        if (viewer == ent.Owner)
+        {
+            AddIcon(args, ent.Comp.StatusIcon);
+        }
+    }
+
+    private void AddIcon(GetStatusIconsEvent args, ProtoId<FactionIconPrototype> iconId)
+    {
+        if (_prototype.TryIndex(iconId, out var iconPrototype))
             args.StatusIcons.Add(iconPrototype);
     }
 }
