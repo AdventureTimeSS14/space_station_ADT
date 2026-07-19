e263f87fa6d08d0ee89ff45ab8abf70ecbeb6bca
CrimeMoot <Wakeafa@gmail.com>
2026-07-19 21:49:54 +0500

    TODO

--- Content.Shared/ADT/Pointing/PointingHelpers.cs
+++ Content.Shared/ADT/Pointing/PointingHelpers.cs
@@ -16,6 +16,7 @@ public static class PointingHelpers
     /// <param name="offsetX">Смещение по X.</param>
     /// <param name="offsetY">Смещение по Y.</param>
     /// <returns>Строка с rich-text разметкой.</returns>
+    /// TODO: Экипированные ВЕЩИ тоже можно указать, но их спрайт будет криво смещён чуть выше. К сожалению не знаю как исправить. Но проблем не возникает.
     public static string BuildPointingChatMessage(string message, int? netEntityId, long size = 16, long scale = 1, long offsetX = 1, long offsetY = 0)
     {
         var escapedMessage = FormattedMessage.EscapeText(message);
