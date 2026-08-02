c955333f51e5651ac76817353e7557f473f02487
CrimeMoot <Wakeafa@gmail.com>
2026-08-02 11:45:02 +0500

    update SilmeGrinder

--- Content.Server/ADT/Xenobiology/SlimeGrinder/SlimeGrinderComponent.cs
+++ Content.Server/ADT/Xenobiology/SlimeGrinder/SlimeGrinderComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared.ADT.Xenobiology.SlimeGrinder;
 
-[RegisterComponent]
+[RegisterComponent, AutoGenerateComponentPause]
 public sealed partial class SlimeGrinderComponent : Component
 {
     /// <summary>
@@ -28,4 +28,14 @@ public sealed partial class SlimeGrinderComponent : Component
     [DataField]
     public SoundSpecifier GrindSound = new SoundPathSpecifier("/Audio/Machines/reclaimer_startup.ogg");
 
+    /// </summary>
+    [DataField]
+    public float AutoFeedRange = 3f;
+
+    [DataField]
+    public TimeSpan ScanInterval = TimeSpan.FromSeconds(1);
+
+    [DataField]
+    [AutoPausedField]
+    public TimeSpan NextScan = TimeSpan.Zero;
 }
