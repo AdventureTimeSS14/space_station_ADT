1c77286847dc8c99586f166c74701319c0d5241d
CrimeMoot <Wakeafa@gmail.com>
2026-08-07 19:59:44 +0500

    Revert "buff megafauna"

--- Content.Shared/Body/Systems/SharedBloodstreamSystem.cs
+++ Content.Shared/Body/Systems/SharedBloodstreamSystem.cs
@@ -18,7 +18,6 @@ using Content.Shared.Popups;
 using Content.Shared.Random.Helpers;
 using Content.Shared.Rejuvenate;
 using Content.Shared.StatusEffectNew;
-using Content.Shared.ADT.Salvage.Components; //ADT-Tweak
 using Content.Shared.ADT.Speech.EntitySystems; //ADT-Tweak
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
@@ -86,7 +85,7 @@ public abstract class SharedBloodstreamSystem : EntitySystem
 
                 // deal bloodloss damage if their blood level is below a threshold.
                 var bloodPercentage = GetBloodLevel(uid);
-                if (bloodPercentage < bloodstream.BloodlossThreshold && !HasComp<MegafaunaComponent>(uid)) // ADT-Tweak: Megafauna bosses do not take damage from blood loss.
+                if (bloodPercentage < bloodstream.BloodlossThreshold)
                 {
                     // bloodloss damage is based on the base value, and modified by how low your blood level is.
                     var amt = bloodstream.BloodlossDamage / (0.1f + bloodPercentage);
