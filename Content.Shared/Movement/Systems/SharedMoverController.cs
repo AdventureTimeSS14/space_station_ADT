7cec5d324f26725deb98a080a08ed9206f22b6d5
Inconnu <177014427+Inconnu1337@users.noreply.github.com>
2026-08-08 10:22:31 +0300

    Трейт позволяющий инвертировать ходьбу и бег (#3085)
    ## Техническая информация
    <!-- Если речь идет об изменении кода, кратко изложите на высоком уровне
    принцип работы нового кода. Перечислите все критические изменения,
    включая изменения пространства имён, публичных классов/методов/полей-
    -->
    - [x] Изменения были протестированы на локальном сервере, и всё работает
    отлично.
    - [x] PR закончен и требует просмотра изменений.
    
    ## Чейнджлог
    :cl: Inconnu
    - add: Добавлен новый трейт позволяющий вам инвертировать вашу ходьбу и
    бег

--- Content.Shared/Movement/Systems/SharedMoverController.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -111,6 +111,7 @@ public abstract partial class SharedMoverController : VirtualController
         InitializeInput();
         InitializeRelay();
         InitializeTileMovement(); // ADT-Tweak
+        InitializeInvertRun(); // ADT tweak
         Subs.CVar(_configManager, CCVars.RelativeMovement, value => _relativeMovement = value, true);
         Subs.CVar(_configManager, CCVars.MinFriction, value => _minDamping = value, true);
         Subs.CVar(_configManager, CCVars.AirFriction, value => _airDamping = value, true);
