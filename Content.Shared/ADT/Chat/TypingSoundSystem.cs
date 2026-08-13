65ba082debf2ff0f9c88c768390143fb6d0926d1
Etvy <semenov.ura_1999@mail.ru>
2026-08-14 00:21:41 +0300

    Апдейт TypingSound

--- Content.Shared/ADT/Chat/TypingSoundSystem.cs
+++ Content.Shared/ADT/Chat/TypingSoundSystem.cs
@@ -1,5 +1,7 @@
 using Content.Shared.Chat;
 using Content.Shared.Chat.TypingIndicator;
+using Content.Shared.Inventory.Events;
+using Content.Shared.Stunnable;
 using JetBrains.Annotations;
 using Robust.Shared.Audio.Systems;
 
@@ -7,7 +9,6 @@ namespace Content.Shared.ADT.Chat;
 
 public sealed partial class TypingSoundSystem : EntitySystem
 {
-
     [Dependency] private SharedAudioSystem _audio = default!;
 
     public override void Initialize()
@@ -16,11 +17,12 @@ public sealed partial class TypingSoundSystem : EntitySystem
 
         SubscribeLocalEvent<TypingSoundComponent, TypingIndicatorStateChangedEvent>(OnTypingStateChanged);
         SubscribeLocalEvent<TypingSoundComponent, EntitySpokeEvent>(OnEntitySpoke);
+        SubscribeLocalEvent<TypingSoundComponent, GotEquippedEvent>(GotEquipped);
+        SubscribeLocalEvent<TypingSoundComponent, GotUnequippedEvent>(GotUnequipped);
     }
 
     private void OnTypingStateChanged(EntityUid uid, TypingSoundComponent component, TypingIndicatorStateChangedEvent args)
     {
-
         if (args.NewState != TypingIndicatorState.Typing)
             return;
 
@@ -30,5 +32,15 @@ public sealed partial class TypingSoundSystem : EntitySystem
     private void OnEntitySpoke(EntityUid uid, TypingSoundComponent component, EntitySpokeEvent args)
     {
         _audio.PlayPvs(component.MessageSentSound, uid);
+    }
+
+    private void GotEquipped(Entity<TypingSoundComponent> ent, ref GotEquippedEvent args)
+    {
+        EnsureComp<TypingSoundComponent>(args.Equipee);
+    }
+
+    private void GotUnequipped(Entity<TypingSoundComponent> ent, ref GotUnequippedEvent args)
+    {
+        RemCompDeferred<TypingSoundComponent>(args.Equipee);
     }
 }
