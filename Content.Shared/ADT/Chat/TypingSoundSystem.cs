098dd9a4838ae47b3ae7f840687c284e68d7b770
Etvy <semenov.ura_1999@mail.ru>
2026-08-14 05:46:13 +0300

    Апдейт TypingSound компонента (#3148)
    ## Описание PR
    Добавлена возможность вешать компонент TypingSound на вещи.
    Когда надеваешь вещь с компонентом TypingSound на пешку, этот компонент
    добавляется пешке, когда снимаешь, то снимается.
    
    no cl no fun

--- Content.Shared/ADT/Chat/TypingSoundSystem.cs
+++ Content.Shared/ADT/Chat/TypingSoundSystem.cs
@@ -1,13 +1,12 @@
 using Content.Shared.Chat;
 using Content.Shared.Chat.TypingIndicator;
-using JetBrains.Annotations;
+using Content.Shared.Inventory.Events;
 using Robust.Shared.Audio.Systems;
 
 namespace Content.Shared.ADT.Chat;
 
 public sealed partial class TypingSoundSystem : EntitySystem
 {
-
     [Dependency] private SharedAudioSystem _audio = default!;
 
     public override void Initialize()
@@ -16,11 +15,12 @@ public sealed partial class TypingSoundSystem : EntitySystem
 
         SubscribeLocalEvent<TypingSoundComponent, TypingIndicatorStateChangedEvent>(OnTypingStateChanged);
         SubscribeLocalEvent<TypingSoundComponent, EntitySpokeEvent>(OnEntitySpoke);
+        SubscribeLocalEvent<TypingSoundComponent, GotEquippedEvent>(GotEquipped);
+        SubscribeLocalEvent<TypingSoundComponent, GotUnequippedEvent>(GotUnequipped);
     }
 
     private void OnTypingStateChanged(EntityUid uid, TypingSoundComponent component, TypingIndicatorStateChangedEvent args)
     {
-
         if (args.NewState != TypingIndicatorState.Typing)
             return;
 
@@ -30,5 +30,17 @@ public sealed partial class TypingSoundSystem : EntitySystem
     private void OnEntitySpoke(EntityUid uid, TypingSoundComponent component, EntitySpokeEvent args)
     {
         _audio.PlayPvs(component.MessageSentSound, uid);
+    }
+
+    private void GotEquipped(Entity<TypingSoundComponent> ent, ref GotEquippedEvent args)
+    {
+        var typingSound = EnsureComp<TypingSoundComponent>(args.Equipee);
+        typingSound.TypingSound = ent.Comp.TypingSound;
+        typingSound.MessageSentSound = ent.Comp.MessageSentSound;
+    }
+
+    private void GotUnequipped(Entity<TypingSoundComponent> ent, ref GotUnequippedEvent args)
+    {
+        RemCompDeferred<TypingSoundComponent>(args.Equipee);
     }
 }
