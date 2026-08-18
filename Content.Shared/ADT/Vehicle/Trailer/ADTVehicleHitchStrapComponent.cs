a3823fdc80a3fce0ac156667ee98ded5c56e4e00
ultradyper <ultradyper@users.noreply.github.com>
2026-08-18 11:35:54 +0000

    feat: сцепка для каталки и мешка для трупов на квадроцикле

---
+++ Content.Shared/ADT/Vehicle/Trailer/ADTVehicleHitchStrapComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.ADT.Vehicle.Trailer;
+
+/// <summary>
+/// Маркер сущности-сцепки транспорта.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ADTVehicleHitchStrapComponent : Component
+{
+}
