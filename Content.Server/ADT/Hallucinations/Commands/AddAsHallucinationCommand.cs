35ba8509ab79a98e015f86d1f51cbcbef8c22f03
_kote <logkedr18@gmail.com>
2026-09-03 03:57:18 +0400

    add(src): Команда, добавляющая сущность А как галлюцинацию сущности Б

---
+++ Content.Server/ADT/Hallucinations/Commands/AddAsHallucinationCommand.cs
@@ -0,0 +1,64 @@
+using System.Linq;
+using Content.Server.ADT.Shizophrenia;
+using Content.Shared.Administration;
+using Content.Shared.EntityEffects.Effects;
+using Content.Shared.EntityEffects.Effects.StatusEffects;
+using Content.Shared.Mind;
+using Content.Shared.Mind.Components;
+using Robust.Shared.Console;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.Administration.Commands;
+
+[AdminCommand(AdminFlags.Admin)]
+sealed partial class AddAsHallucinationCommand : IConsoleCommand
+{
+    [Dependency] private IEntityManager _entManager = default!;
+    [Dependency] private IPrototypeManager _proto = default!;
+
+    public string Command => "add-as-hallucination";
+
+    public string Description => Loc.GetString("add-as-hallucination-command-description");
+
+    public string Help => Loc.GetString("add-as-hallucination-command-help-text", ("command", Command));
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length < 4)
+        {
+            shell.WriteLine(Loc.GetString("shell-wrong-arguments-number"));
+            return;
+        }
+
+        if (!EntityUid.TryParse(args[0], out var target))
+        {
+            shell.WriteLine(Loc.GetString("shell-entity-uid-must-be-number"));
+            return;
+        }
+        if (!EntityUid.TryParse(args[1], out var toAdd))
+        {
+            shell.WriteLine(Loc.GetString("shell-entity-uid-must-be-number"));
+            return;
+        }
+
+        var hall = _entManager.System<SchizophreniaSystem>();
+        hall.AddAsHallucination(target, toAdd);
+        shell.WriteLine(Loc.GetString("add-as-hallucination-command-success", ("target", target), ("added", toAdd)));
+    }
+
+    public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        if (args.Length == 1)
+        {
+            var opts = _entManager.AllEntities<CanHallucinateComponent>().Select(ent => new CompletionOption(ent.Owner.ToString(), _entManager.ToPrettyString(ent))).ToList();
+            return CompletionResult.FromHintOptions(opts, "<target>");
+        }
+
+        if (args.Length == 2)
+        {
+            return CompletionResult.FromHint("<entity to add>");
+        }
+
+        return CompletionResult.Empty;
+    }
+}
