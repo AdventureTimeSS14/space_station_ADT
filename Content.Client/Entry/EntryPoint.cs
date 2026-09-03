3aa815a2c370cb7d3fab48f9fe5fab2ff99634f0
_kote <logkedr18@gmail.com>
2026-09-03 04:16:47 +0400

    fix(src): Игнорирование прототипов галлюцинаций на клиенте

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -148,6 +148,7 @@ namespace Content.Client.Entry
             _prototypeManager.RegisterIgnore("sponsorLoadout"); // ADT-Sponsor-Loadout
             _prototypeManager.RegisterIgnore("sponsorPersonalLoadout"); // ADT-Sponsor-Loadout
             _prototypeManager.RegisterIgnore("sponsorLoadoutTierSetting"); // ADT-Sponsor-Loadout
+            _prototypeManager.RegisterIgnore("hallucinationsPack"); // ADT-Tweak
 
             _componentFactory.GenerateNetIds();
             _adminManager.Initialize();
