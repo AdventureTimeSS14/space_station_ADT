e12d608bf00c8d8c1278a22831bfbe0b02fc4d1c
CrimeMoot <Wakeafa@gmail.com>
2026-09-11 23:07:04 +0500

    fix xenoslime Zombies

--- Content.Server/ADT/Xenobiology/SlimeLatchSystem.cs
+++ Content.Server/ADT/Xenobiology/SlimeLatchSystem.cs
@@ -32,6 +32,7 @@ using Robust.Shared.Physics.Systems;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Random;
 using Content.Server.Speech.Components;
+using Content.Shared.Zombies;
 using System.Linq;
 
 namespace Content.Server.ADT.Xenobiology.Systems;
@@ -316,6 +317,7 @@ public sealed partial class SlimeLatchSystem : EntitySystem
     public bool CanLatch(Entity<SlimeComponent> ent, EntityUid target)
     {
         return !(IsLatched(ent)
+            || HasComp<ZombieComponent>(ent)
             || _mobState.IsDead(target)
             || !_actionBlocker.CanInteract(ent, target)
             || !HasComp<MobStateComponent>(target)
