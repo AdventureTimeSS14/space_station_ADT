374010bd2706844485da83061a7ea049ed5f31de
Cтаніслав <mokry@MacBookPro.lan>
2026-09-16 14:18:26 +0200

    Add multishot system for dual-wielding pistols. Добавиk систему  стрельбы с двух рук для пистолетов.

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -43,6 +43,7 @@ using Content.Shared.ADT.Weapons.Ranged.WearableGun;
 using Content.Shared.Electrocution;
 using Content.Shared.ADT.Crawling.Components;
 using Content.Shared.Inventory.VirtualItem;
+using Content.Goobstation.Common.Weapons.Multishot;
 
 namespace Content.Shared.Weapons.Ranged.Systems;
 
@@ -160,6 +161,12 @@ public abstract partial class SharedGunSystem : EntitySystem
 
     private void OnShootRequest(RequestShootEvent msg, EntitySessionEventArgs args)
     {
+        // Goobstation - Multishot - Ensures that guns shooting at same time.
+        var gunUid = GetEntity(msg.Gun);
+
+        if (HasComp<MultishotComponent>(gunUid))
+            return;
+        // Goobstation - End
         var user = args.SenderSession.AttachedEntity;
 
         if (user == null ||
@@ -497,6 +504,8 @@ public abstract partial class SharedGunSystem : EntitySystem
 
         if (shooterEv.Push)
             CauseImpulse(fromCoordinates, toCoordinates.Value, (user, userPhysics));
+        
+        UpdateAmmoCount(gun); // Goobstation tweak - Update ammo count after shooting to ensure the ammo count is correct after shooting.
         return true;
     }
 
