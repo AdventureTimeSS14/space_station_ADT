7834cceb8658fa5e12b007dc47d50886bcd5dd66
Inconnu <177014427+Inconnu1337@users.noreply.github.com>
2026-09-20 11:57:38 +0300

    Update ADTAshWalkerEvents.cs

--- Content.Shared/ADT/AshWalker/ADTAshWalkerEvents.cs
+++ Content.Shared/ADT/AshWalker/ADTAshWalkerEvents.cs
@@ -1,4 +1,6 @@
 using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.ADT.AshWalker;
 
@@ -16,3 +18,8 @@ public sealed partial class ADTNecropolisCompassActionEvent : InstantActionEvent
 
 [ByRefEvent]
 public record struct ADTHealTouchUsedEvent(EntityUid User, bool Handled = false);
+
+[Serializable, NetSerializable]
+public sealed partial class ADTNecropolisCompassDoAfterEvent : SimpleDoAfterEvent
+{
+}
