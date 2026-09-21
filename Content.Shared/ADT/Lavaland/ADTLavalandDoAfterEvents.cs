897d685bced40fc66abaf930e2b20db7df09b3c1
Inconnu <177014427+Inconnu1337@users.noreply.github.com>
2026-09-21 03:01:47 +0300

    rm shit gate

--- Content.Shared/ADT/Lavaland/ADTLavalandDoAfterEvents.cs
+++ Content.Shared/ADT/Lavaland/ADTLavalandDoAfterEvents.cs
@@ -7,8 +7,3 @@ namespace Content.Shared.ADT.Lavaland;
 public sealed partial class ADTGraceCutDoAfterEvent : SimpleDoAfterEvent
 {
 }
-
-[Serializable, NetSerializable]
-public sealed partial class ADTNecropolisGateDoAfterEvent : SimpleDoAfterEvent
-{
-}
