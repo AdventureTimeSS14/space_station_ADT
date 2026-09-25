18e9506b5c0fcae5369a37511279d8b45a31690a
CrimeMoot <Wakeafa@gmail.com>
2026-09-25 20:38:19 +0500

    VisibilityThreshold 0.7

--- Content.Shared/ADT/Medical/ADTHealingVisualsSystem.cs
+++ Content.Shared/ADT/Medical/ADTHealingVisualsSystem.cs
@@ -11,7 +11,7 @@ public sealed partial class ADTHealingVisualsSystem : EntitySystem
 {
     [Dependency] private readonly SharedStealthSystem _stealth = default!;
 
-    private const float StealthVisibilityThreshold = 0.3f;
+    private const float StealthVisibilityThreshold = 0.7f;
 
     public bool TryStartHealEffect(EntityUid target, EntProtoId? effect)
     {
